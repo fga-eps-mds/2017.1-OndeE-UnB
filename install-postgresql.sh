73bbf5a3ba69731bf8b52223310f40c4ec0a24b4
eduqg <eduardoqgomes@gmail.com>
2017-03-16 22:50:47 -0300

    Inserting Virtual Machine with vagrant with ruby on rails environment

---
+++ install-postgresql.sh
@@ -0,0 +1,3 @@
+sudo sh -c 'echo "deb http://apt.postgresql.org/pub/repos/apt/ `lsb_release -cs`-pgdg main" >> /etc/apt/sources.list.d/pgdg.list'
+
+wget -q https://www.postgresql.org/media/keys/ACCC4CF8.asc -O - | sudo apt-key add -
