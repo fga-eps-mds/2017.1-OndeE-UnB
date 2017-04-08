f01fcedfa85c30c9e925aff391a4a023b42b6486
Jordan Miranda <jordan.oliveira.m@gmail.com>
2017-04-08 16:21:59 -0300

    added bootstrap resource

--- darcyWeb/app/controllers/home_controller.rb
+++ darcyWeb/app/controllers/home_controller.rb
@@ -1,6 +1,5 @@
-class HomeController < ActionController::Base
+class HomeController < ApplicationController
     protect_from_forgery with: :exception
     def home
-        render html: "hello, world!"
     end
 end
