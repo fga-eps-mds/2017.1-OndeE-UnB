ed739ab6b4634fea01cae142c25286b18600d6ca
rafaelrabetti <leafar_rabetti@hotmail.com>
2017-04-09 19:52:23 -0300

    modularizing views, creating about view, and working on navbar and footer

--- darcyWeb/config/routes.rb
+++ darcyWeb/config/routes.rb
@@ -1,11 +1,13 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-
   root 'map#index'
 
   root 'buildings#index'
 
   resources :buildings
+
+  get "app/views/map/index.html.erb", to: "map#index", as: "map"
+  get "app/views/about/about.html.erb", to: "about#about", as: "about"
 
 end
