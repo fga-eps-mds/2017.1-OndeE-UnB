b6147a775daf5b5561070dd1556d01267cd3a90c
Daniel <danmoura17@gmail.com>
2017-04-14 18:37:33 +0000

    Including route between two points

--- darcyWeb/app/assets/javascripts/map.js
+++ darcyWeb/app/assets/javascripts/map.js
@@ -44,19 +44,60 @@ var infoLabel = {
     }
 }
 
-var centerMap = [-15.764544, -47.866929];
+var centerMap = [-15.762023, -47.867114];
+var bce = [-15.761096, -47.867648];
+var reitoria = [-15.762795, -47.866854];
 
 var map = L.map('map', {
     center: centerMap,
     zoom: 18,
-    minZoom: 16
+    minZoom: 16,
 });
 
 L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
     attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
 }).addTo(map);
 
+L.marker(reitoria).addTo(map)
+    .bindPopup('Reitoria');
 
+L.marker(bce).addTo(map)
+    .bindPopup('BCE');
 
-L.marker(centerMap).addTo(map)
-    .bindPopup('Onde É? UnB');
+var routeLayer = L.featureGroup().addTo(map);
+
+var getRoutes = function() {
+
+        routeLayer.clearLayers();
+
+        // you need to define some options for the polygon service
+        // for more travel options check out the other tutorials
+        var travelOptions = r360.travelOptions();
+        // we only have one source which is the marker we just added
+        travelOptions.addSource(centerMap);
+        // add two targets to the options
+        travelOptions.addTarget(bce);
+        travelOptions.addTarget(reitoria);
+        // set the travel type to transit
+        travelOptions.setTravelType('walk');
+        // please contact us and request your own key
+        travelOptions.setServiceKey('F2H7U2QUY7P3AESM3O20K2U');
+        // set the service url for your area
+        travelOptions.setServiceUrl('https://service.route360.net/germany/');
+
+        // start the service
+        r360.RouteService.getRoutes(travelOptions, function(routes) {
+
+          // one route for each source and target combination
+          routes.forEach(function(route) {
+
+            r360.LeafletUtil.fadeIn(routeLayer, route, 1000, "travelDistance");
+          });
+        });
+      }
+
+      getRoutes();
+
+      centerMap.on('dragend', getRoutes);
+      bce.on('dragend', getRoutes);
+      reitoria.on('dragend', getRoutes);
