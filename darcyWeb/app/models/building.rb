46cf930a29d9191af39f3cc259ea43388a989eda
Lucas S <lucas.soaresouza@gmail.com>
2017-04-15 18:56:40 +0000

    starting unit tests in buildings and locations models using the gem Rspec 3.5

--- darcyWeb/app/models/building.rb
+++ darcyWeb/app/models/building.rb
@@ -1,3 +1,9 @@
 class Building < ApplicationRecord
   acts_as :location
+  validates :acronym, presence: true
+  validates :phone, presence: true
+  validates :title, presence: true
+  validates :latitude, presence: true
+  validates :longitude, presence: true
+  validates :geo_data, presence: true   
 end
