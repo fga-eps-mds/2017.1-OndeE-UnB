88005cf9098757096c8e5b657de1cf9d8def885b
Kairon Velozo <kairon333@gmail.com>
2017-04-15 02:05:19 -0300

    Setting application to pt-BR, creating the i18n for building name and organizing the leaflet folder structure.

--- darcyWeb/app/assets/javascripts/leaflet/map.js
+++ darcyWeb/app/assets/javascripts/leaflet/map.js
@@ -1,10 +1,11 @@
-var centerMap = [-15.764544, -47.866929];
+// This is the common setting to starting up the map
+
+const centerMap = [-15.764544, -47.866929];
 
 var map = L.map('map', {
     center: centerMap,
     zoom: 18,
-    minZoom: 16,
-    maxZoom: 25
+    minZoom: 16
 });
 
 L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
