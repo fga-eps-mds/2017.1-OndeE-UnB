06e451828d5afb768589054830f69129abfb9cd9
Lucas S <lucas.soaresouza@gmail.com>
2017-04-15 20:08:41 +0000

    applying tests of view

---
+++ darcyWeb/spec/controllers/admin_controller_spec.rb
@@ -0,0 +1,12 @@
+require 'rails_helper'
+
+describe AdminController do
+
+	describe "checking the request the view of admin" do
+		it "checks the request" do
+			get:index
+			expect(response).to render_template("index")
+		end
+	end
+
+end
