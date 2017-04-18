1ca5317c370a1065c8a27409fa503bf995a801f0
Alexandre Kryonidis <alexandrekry@gmail.com>
2017-04-18 01:15:30 +0000

    Adding some acceptance tests to the home_page and admin dashboard

--- darcyWeb/features/step_definitions/view_map_steps.rb
+++ darcyWeb/features/step_definitions/view_map_steps.rb
@@ -1,9 +1,8 @@
 Given(/^I am a normal user$/) do
-  #if  user_signed_in?
-  #	@user.update_attributes(current_sign_in_token: "")
-  #else
-  #	# User logged out
-  #end
+end
+
+Given(/^I wait for the map to appear$/) do 
+  have_xpath("//div[@class='leaflet-map-pane']")
 end
 
 When(/^I go to the homepage$/) do
@@ -11,5 +10,64 @@ When(/^I go to the homepage$/) do
 end
 
 Then(/^I should see the map$/) do
-
+  begin
+    field = find_by_id("map")
+  rescue Capybara::ElementNotFound
+    puts "map id not found in homepage"
+    return false
+  end
 end
+
+#Then(/^I should see zoom in and out buttons$/) do
+#   puts find_all("//a[@class='leaflet-control-zoom-in']")
+#   find_all("//a[@class='leaflet-control-zoom-out']")
+#end
+
+
+Then(/^I should see the buttons to change the floor$/) do
+  find('.level button', :text => 'Subsolo').click
+  find('.level button', :text => '1').click
+  find('.level button', :text => '2').click
+  find('.level button', :text => '3').click
+end
+
+
+
+# When(/^I click on the zoom in$/) do
+
+#   find_all("//a[@class='leaflet-control-zoom-out']").map {
+#     |zoomOut|
+#     puts zoomOut
+#     zoomOut.click
+#   }
+
+#   a = find_all("//div[@class='leaflet-proxy']").map {
+#     |ab|
+#     puts ab
+#   }
+
+
+#   find_all("//a[@class='leaflet-control-zoom-in']").map {
+#     |zoomIn|
+#     zoomIn.click
+#   }
+# end
+
+# Then(/^the map should be zoomed in$/) do
+#   #puts @helloa
+# end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- darcyWeb/features/view_map.feature
+++ darcyWeb/features/view_map.feature
@@ -3,7 +3,17 @@ Feature: View Unb Map
 	As a normal user
 	I want to view the university map
 
-	Scenario: View Map
+	Scenario: View the Map
 		Given I am a normal user
 		When I go to the homepage
+		And  I wait for the map to appear
 		Then I should see the map
+		#And I should see zoom in and out buttons
+		And I should see the buttons to change the floor
+
+
+	#Scenario: Zoom in the map
+	#	Given I go to the homepage
+	#	And I wait for the map to appear
+	#	When I click on the zoom in
+	#	Then the map should be zoomed in
