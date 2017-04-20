5218f2fb5794eddf711332385d3d2fbd5c0b0d9f
Alexandre Kryonidis <alexandrekry@gmail.com>
2017-04-20 00:38:09 +0000

    Fixing acceptance test after merger with admin authentication branch

--- darcyWeb/features/step_definitions/manage_contents_steps.rb
+++ darcyWeb/features/step_definitions/manage_contents_steps.rb
@@ -1,5 +1,13 @@
 Given(/^I am an admin user$/) do
-	# logged in
+  email = 'testing@man.net'
+  password = 'secretpass'
+  Admin.new(:email => email, :password => password, :password_confirmation => password).save!
+
+  visit '/admins/sign_in'
+  fill_in "admin_email", :with => email
+  fill_in "admin_password", :with => password
+  click_button "Log in"
+
 end
 
 
@@ -25,6 +33,53 @@ end
 Then(/^I should see a link to manage rooms$/) do
 	find('.title', :text => 'Salas')
 end
+
+
+
+
+Given(/^I am an not an admin user$/) do
+  current_driver = Capybara.current_driver
+  begin
+    Capybara.current_driver = :rack_test
+    page.driver.submit :delete, "admins/sign_out", {}
+  ensure
+    Capybara.current_driver = current_driver
+  end
+end
+
+Then(/^I should be redirected to the login page$/) do
+	expect(page).to have_content("Log in")
+	expect(page).to have_content("Forgot your password")
+end
+
+
+
+
+When(/^I click on the departments link$/) do
+  find('a', :text => 'Departamentos').click
+end
+
+Then(/^I should go to the departments index page$/) do
+  expected = admin_buildings_path
+  actual = URI.parse(current_url).path
+
+  # to be, equal  -> same type
+  expect(actual).to eql(expected)
+end
+
+
+When(/^I click on the buildings link$/) do
+  find('a', :text => 'Edifícios').click
+end
+
+Then(/^I should go to the buildings index page$/) do
+  expected = admin_buildings_path
+  actual = URI.parse(current_url).path
+  expect(actual).to eql(expected)
+end
+
+
+
 
 
 #http://localhost:8080/admin/index
