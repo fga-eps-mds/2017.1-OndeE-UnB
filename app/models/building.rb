52e9232272cce3ec6f2076f7fe13b7233e0c416a
Lucas S <lucas.soaresouza@gmail.com>
2017-04-20 00:52:20 +0000

    Testing the validations of the attributes of building

--- app/models/building.rb
+++ app/models/building.rb
@@ -1,3 +1,5 @@
+require 'json'
+
 class Building < ApplicationRecord
   has_and_belongs_to_many :departments
   has_many :rooms
@@ -5,8 +7,11 @@ class Building < ApplicationRecord
   acts_as :location
   validates :acronym, length: {  maximum: 20 }, presence: true
   validates :title, length: {  maximum: 50 }, presence: true
-  validates :phone, numericality: { only_integer: true }, presence: true
+  validates :phone, length: { maximum: 11 }, numericality: { only_integer: true }
   validates :latitude, numericality: true, presence: true
   validates :longitude, numericality: true, presence: true
   validates :geo_data, presence: true
+
 end
+
+
