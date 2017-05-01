6db434552bb3c6e105a96e6d8fba37dba5fb29e6
Kairon Velozo <kairon333@gmail.com>
2017-05-01 17:12:55 -0300

    Changing the map tile.

--- app/assets/javascripts/leaflet/map.js
+++ app/assets/javascripts/leaflet/map.js
@@ -8,7 +8,9 @@ var map = L.map('map', {
     minZoom: 16
 });
 
-L.tileLayer('https://api.mapbox.com/styles/v1/kaironvzb/cj1m79xc9000p2sqrhrjdhel2/tiles/256/{z}/{x}/{y}?access_token=pk.eyJ1Ijoia2Fpcm9udnpiIiwiYSI6ImNpd21mbW0wbDAwNXMyenFpanlmbHZ6ZXAifQ.RtMAGQj_0ho54Rw6D812hw', {
+var url = "https://api.mapbox.com/styles/v1/kaironvzb/cj1y3dkki00042sn074lbuo5k/tiles/256/{z}/{x}/{y}?access_token=pk.eyJ1Ijoia2Fpcm9udnpiIiwiYSI6ImNpd21mbW0wbDAwNXMyenFpanlmbHZ6ZXAifQ.RtMAGQj_0ho54Rw6D812hw";
+
+L.tileLayer(url, {
     attribution: '&copy; <a href="https://www.mapbox.com/map-feedback/">Mapbox</a>\
                   &copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
 }).addTo(map);
