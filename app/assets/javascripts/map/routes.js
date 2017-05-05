382253dc18df65e32003abd4bc6e4bba6e21f23a
Kairon Velozo <kairon333@gmail.com>
2017-05-05 12:38:50 -0300

    Adding items to calculate route in the context menu.

--- app/assets/javascripts/map/routes.js
+++ app/assets/javascripts/map/routes.js
@@ -2,11 +2,10 @@
 //= require leaflet/lrm-mapzen
 
 L.easyButton('fa-map-marker', function(btn, map) {
-  if (sidebar.isVisible()){
+  if (sidebar.isVisible()) {
     sidebar.hide();
-  }
-  else {
-    $("#sidebar").load( "/map/routes", function() {
+  } else {
+    $("#sidebar").load("/map/routes", function() {
       sidebar.show();
     });
   }
@@ -17,8 +16,32 @@ L.Routing.control({
     L.latLng(-15.762023, -47.867114),
     L.latLng(-15.761096, -47.867648)
   ],
- router: L.Routing.mapzen('mapzen-CEq2eYW', {
-   costing:'pedestrian'
- }),
- formatter: new L.Routing.mapzenFormatter()
+  router: L.Routing.mapzen('mapzen-CEq2eYW', {
+    costing: 'pedestrian'
+  }),
+  formatter: new L.Routing.mapzenFormatter()
 }).addTo(map);
+
+
+map.contextmenu.enable();
+map.contextmenu.addItem({
+  text: 'Rotas a partir daqui',
+  callback: routesFromHere
+});
+
+map.contextmenu.addItem({
+  text: 'Rotas para cá',
+  callback: routesToHere
+});
+
+function routesFromHere(e) {
+  var lat = e.latlng.lat;
+  var lng = e.latlng.lng;
+  $('#route_origin').val( lat + ", " + lng);
+  control.spliceWaypoints(0, 1, e.latlng);
+}
+
+function routesToHere(e) {
+  $('#route_destination').val(e.latlng);
+  control.spliceWaypoints(0, 1, e.latlng);
+}
