b9488284cd236cbeca6f3e2d623f7f61c9840aca
Stefane Souza <stefanesouza04@gmail.com>
2017-05-05 23:57:55 +0000

    Added courses parse

--- app/controllers/parser_controller.rb
+++ app/controllers/parser_controller.rb
@@ -1,6 +1,6 @@
 class ParserController < ApplicationController
   def index
-    render plain: departaments
+    render plain: courses
   end
 
   def departaments
@@ -16,5 +16,28 @@ class ParserController < ApplicationController
       departments_urls.push(department['href'])
     end
     return departments_urls
+  end
+
+  def courses
+    require 'nokogiri'
+    require 'open-uri'
+    puts "estou aqui"
+    site = 'https://matriculaweb.unb.br/graduacao/'
+    courses_urls = []
+    departaments.each do |departament|
+      #departament courses
+      html = open(site+departament)
+      url = site+departament
+      puts url
+      doc = Nokogiri::HTML(html, 'utf-8')
+      rows = doc.css('.FrameCinza tr')
+      rows.shift
+
+      rows.each_with_index do |row, index|
+        course = row.at('td[2] a')
+        courses_urls.push(course['href'])
+      end
+    end
+    return courses_urls
   end
 end
