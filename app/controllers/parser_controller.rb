b4cea3c65add2de1f9d6d932b08cd09dee852a11
tayh <tayhcarvalho@gmail.com>
2017-05-06 01:20:08 +0000

    Creating the get Data function

--- app/controllers/parser_controller.rb
+++ app/controllers/parser_controller.rb
@@ -1,6 +1,6 @@
 class ParserController < ApplicationController
   def index
-    render plain: courses
+    render plain: datas
   end
 
   def departaments
@@ -21,7 +21,6 @@ class ParserController < ApplicationController
   def courses
     require 'nokogiri'
     require 'open-uri'
-    puts "estou aqui"
     site = 'https://matriculaweb.unb.br/graduacao/'
     courses_urls = []
     departaments.each do |departament|
@@ -39,5 +38,25 @@ class ParserController < ApplicationController
       end
     end
     return courses_urls
+  end
+  def datas
+    require 'nokogiri'
+    require 'open-uri'
+    site = 'https://matriculaweb.unb.br/graduacao/'
+    datas_urls = []
+    puts 'estou aqui'
+    datas.each do |datas|
+      html = open(site+departament+courses)
+      url = site+departament+courses
+      puts url
+      doc = Nokogiri::HTML(html, 'utf-8')
+      rows = doc.css('.FrameCinza tr')
+      rows.shift
+      rows.each_with_index do |row, index|
+        data = row.at('td[4] a')
+        datas_urls.push(data['href'])
+      end
+    end
+    return datas_urls
   end
 end
