bb39a142e5e2f01d9889614f03813fe99da0a39f
Kairon Velozo <kairon333@gmail.com>
2017-05-10 20:54:34 -0300

    Adding tests to map.

--- app/assets/javascripts/map/routes.js
+++ app/assets/javascripts/map/routes.js
@@ -108,118 +108,116 @@ L.easyButton('ion-merge', function(btn, map) {
 
 }).addTo(map);
 
-// // loads the routes form into the sidebar
-// function loadRouteForm(data) {
-//
-//   $("#sidebar").load("/map/routes", function() {
-//
-//     $('.btn-reverse-route').on('click', function(e) {
-//       reverseRoute(e);
-//     });
-//
-//
-//     // set elements, so jquery can look up
-//     route_form = function() {
-//       this.form = $('#sidebar').find('form');
-//       this.origin = this.form.find('input[name="route[origin]"]');
-//       this.destination = this.form.find('input[name="route[destination]"]');
-//       this.mode = this.form.find('input[name="route[mode]"]');
-//       this.submit = this.form.find('button[type=submit]');
-//       return this;
-//     }.call({});
-//
-//     fillFormRouteLocations(data);
-//
-//     // calculate route when user clicks submit button
-//     route_form.submit.on('click', function(e) {
-//       // prevent default behavior
-//       e.preventDefault();
-//
-//       // get route mode from the form and set into the control
-//       control.options.router.options.costing = route_form.mode.parent('.btn.active').find('input').val();
-//
-//       var origin_latlng = control.getWaypoints()[0].latLng;
-//       var destination_latlng = control.getWaypoints()[1].latLng;
-//
-//       if (origin_latlng != null && destination_latlng != null) {
-//         control.route();
-//       }
-//
-//     });
-//
-//     sidebar.show();
-//   });
-// }
-//
-// function unLoadRoute() {
-//   sidebar.hide();
-//
-//   // remove markers from map
-//   removeMarker(origin);
-//   removeMarker(destination);
-//
-//   // removes route from map
-//   control.spliceWaypoints(0, 2);
-//
-//   // disable auto route
-//   control.options.autoRoute = false;
-// }
-//
-// // set values to location in the route form
-// // when sidebar is already loaded
-// function fillFormRouteLocations(data) {
-//   if ('origin' in data) {
-//     route_form.origin.val(data.origin);
-//   }
-//   if ('destination' in data) {
-//     route_form.destination.val(data.destination);
-//   }
-// }
-//
-//
-// function setRouteLocation(e, waypoint) {
-//   var lat = e.latlng.lat;
-//   var lng = e.latlng.lng;
-//
-//   var data = {
-//     [waypoint.title]: lat + ", " + lng
-//   };
-//
-//   if (sidebar.isVisible()) {
-//     fillFormRouteLocations(data);
-//   } else {
-//     loadRouteForm(data);
-//   }
-//   if (waypoint.marker == null) {
-//     createMarker(waypoint, e.latlng);
-//   } else {
-//     waypoint.marker.setLatLng(e.latlng);
-//   }
-// };
-//
-// function removeMarker(waypoint) {
-//   try {
-//     map.removeLayer(waypoint.marker);
-//     waypoint.marker = null;
-//   }
-//   catch(err) {
-//     console.error(err.message);
-//   }
-// }
-//
-// function createMarker(waypoint, latlng) {
-//   console.log('Create marker');
-//   console.log(waypoint);
-//   console.log(latlng);
-//   waypoint.marker = L.marker(latlng, {
-//     icon: L.AwesomeMarkers.icon({
-//       prefix: 'ion',
-//       icon: waypoint.icon,
-//       markerColor: waypoint.color
-//     })
-//   });
-//   map.addLayer(waypoint.marker);
-// }
+// loads the routes form into the sidebar
+function loadRouteForm(data) {
+
+  $("#sidebar").load("/map/routes", function() {
+
+    $('.btn-reverse-route').on('click', function(e) {
+      reverseRoute(e);
+    });
+
+
+    // set elements, so jquery can look up
+    route_form = function() {
+      this.form = $('#sidebar').find('form');
+      this.origin = this.form.find('input[name="route[origin]"]');
+      this.destination = this.form.find('input[name="route[destination]"]');
+      this.mode = this.form.find('input[name="route[mode]"]');
+      this.submit = this.form.find('button[type=submit]');
+      return this;
+    }.call({});
+
+    fillFormRouteLocations(data);
+
+    // calculate route when user clicks submit button
+    route_form.submit.on('click', function(e) {
+      // prevent default behavior
+      e.preventDefault();
+
+      // get route mode from the form and set into the control
+      control.options.router.options.costing = route_form.mode.parent('.btn.active').find('input').val();
+
+      var origin_latlng = control.getWaypoints()[0].latLng;
+      var destination_latlng = control.getWaypoints()[1].latLng;
+
+      if (origin_latlng != null && destination_latlng != null) {
+        control.route();
+      }
+
+    });
+
+    sidebar.show();
+  });
+}
+
+function unLoadRoute() {
+  sidebar.hide();
+
+  // remove markers from map
+  removeMarker(origin);
+  removeMarker(destination);
+
+  // removes route from map
+  control.spliceWaypoints(0, 2);
+
+  // disable auto route
+  control.options.autoRoute = false;
+}
+
+// set values to location in the route form
+// when sidebar is already loaded
+function fillFormRouteLocations(data) {
+  if ('origin' in data) {
+    route_form.origin.val(data.origin);
+  }
+  if ('destination' in data) {
+    route_form.destination.val(data.destination);
+  }
+}
+
+function setRouteLocation(e, waypoint) {
+  var lat = e.latlng.lat;
+  var lng = e.latlng.lng;
+
+  var data = {};
+  data[waypoint.title] = lat + ", " + lng;
+
+  if (sidebar.isVisible()) {
+    fillFormRouteLocations(data);
+  } else {
+    loadRouteForm(data);
+  }
+  if (waypoint.marker == null) {
+    createMarker(waypoint, e.latlng);
+  } else {
+    waypoint.marker.setLatLng(e.latlng);
+  }
+};
+
+function removeMarker(waypoint) {
+  try {
+    map.removeLayer(waypoint.marker);
+    waypoint.marker = null;
+  }
+  catch(err) {
+    console.error(err.message);
+  }
+}
+
+function createMarker(waypoint, latlng) {
+  console.log('Create marker');
+  console.log(waypoint);
+  console.log(latlng);
+  waypoint.marker = L.marker(latlng, {
+    icon: L.AwesomeMarkers.icon({
+      prefix: 'ion',
+      icon: waypoint.icon,
+      markerColor: waypoint.color
+    })
+  });
+  map.addLayer(waypoint.marker);
+}
 
 // this is performed when user clicks "Rotas a partir daqui"
 function routesFromHere(e) {
@@ -232,40 +230,40 @@ function routesToHere(e) {
   setRouteLocation(e, destination);
   control.spliceWaypoints(control.getWaypoints().length - 1, 1, e.latlng);
 }
-//
-// function reverseRoute(e) {
-//   e.preventDefault();
-//
-//   // reverse waypoints to route
-//   var waypoints = control.getWaypoints();
-//   control.setWaypoints(waypoints.reverse());
-//
-//   // NOTE reverse markers was so hard to figure out
-//   if (origin.marker != null && destination.marker == null) {
-//     console.log('Destination in blank.');
-//     createMarker(destination, origin.marker.getLatLng());
-//     removeMarker(origin);
-//   }
-//   else if (destination.marker != null && origin.marker == null) {
-//     console.log('Origin in blank');
-//     createMarker(origin, destination.marker.getLatLng());
-//     removeMarker(destination);
-//   }
-//
-//   if(origin.marker != null && destination.marker != null) {
-//     var latlng = origin.marker.getLatLng();
-//     origin.marker.setLatLng(destination.marker.getLatLng());
-//     destination.marker.setLatLng(latlng);
-//   }
-//
-//   var origin_latlng = route_form.origin.val();
-//   var destination_latlng = route_form.destination.val();
-//
-//   // swap values in form
-//   route_form.origin.val(destination_latlng);
-//   route_form.destination.val(origin_latlng);
-//
-// }
-// // TODO Require to fill out origin and destination in the form, before calculate route
-// // TODO Add button to create a new route
-// // TODO Suggest locations on whe form
+
+function reverseRoute(e) {
+  e.preventDefault();
+
+  // reverse waypoints to route
+  var waypoints = control.getWaypoints();
+  control.setWaypoints(waypoints.reverse());
+
+  // NOTE reverse markers was so hard to figure out
+  if (origin.marker != null && destination.marker == null) {
+    console.log('Destination in blank.');
+    createMarker(destination, origin.marker.getLatLng());
+    removeMarker(origin);
+  }
+  else if (destination.marker != null && origin.marker == null) {
+    console.log('Origin in blank');
+    createMarker(origin, destination.marker.getLatLng());
+    removeMarker(destination);
+  }
+
+  if(origin.marker != null && destination.marker != null) {
+    var latlng = origin.marker.getLatLng();
+    origin.marker.setLatLng(destination.marker.getLatLng());
+    destination.marker.setLatLng(latlng);
+  }
+
+  var origin_latlng = route_form.origin.val();
+  var destination_latlng = route_form.destination.val();
+
+  // swap values in form
+  route_form.origin.val(destination_latlng);
+  route_form.destination.val(origin_latlng);
+
+}
+// TODO Require to fill out origin and destination in the form, before calculate route
+// TODO Add button to create a new route
+// TODO Suggest locations on whe form
