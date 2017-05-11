2a3e628790c4f6bcc30d2aad4b76aa425c31de95
tayh <tayhcarvalho@gmail.com>
2017-05-11 02:16:14 +0000

    Creating get data function

--- darcyWeb/app/controllers/parser_controller.rb
+++ darcyWeb/app/controllers/parser_controller.rb
@@ -3,6 +3,7 @@ class ParserController < ApplicationController
     render plain: datas
   end
 
+# Departments parser
   def departaments
     require 'nokogiri'
     require 'open-uri'
@@ -11,52 +12,59 @@ class ParserController < ApplicationController
     rows = doc.css('.FrameCinza tr')
     rows.shift
     departments_urls = []
+    # Getting Department links
     rows.each_with_index do |row, index|
       department = row.at('td[3] a')
       departments_urls.push(department['href'])
+      puts "Departamento: " + department['href']
     end
     return departments_urls
   end
 
+# Courses parser
   def courses
     require 'nokogiri'
     require 'open-uri'
     site = 'https://matriculaweb.unb.br/graduacao/'
     courses_urls = []
+
     departaments.each do |departament|
-      #departament courses
       html = open(site+departament)
       url = site+departament
-      puts url
+      puts "Departamento:" + url
       doc = Nokogiri::HTML(html, 'utf-8')
       rows = doc.css('.FrameCinza tr')
       rows.shift
 
+      #Getting Courses links
       rows.each_with_index do |row, index|
         course = row.at('td[2] a')
         courses_urls.push(course['href'])
+        puts "Cursos: " + course['href']
       end
     end
     return courses_urls
   end
+
+  #Data parser
   def datas
     require 'nokogiri'
     require 'open-uri'
-    site = 'https://matriculaweb.unb.br/graduacao/'
+    site = 'https://matriculaweb.unb.br/graduacao/' # oferta_dados
     datas_urls = []
-    puts 'estou aqui'
-    datas.each do |datas|
-      html = open(site+departament+courses)
-      url = site+departament+courses
-      puts url
+    courses.each do |course|
+      html = open(site+course)
+      url = site+course
+      puts "Curso: " + url
       doc = Nokogiri::HTML(html, 'utf-8')
-      rows = doc.css('.FrameCinza tr')
+      rows = doc.css('div')
       rows.shift
+      datas_urls = []
+      #Getting the time and places of courses
       rows.each_with_index do |row, index|
-        data = row.at('td[4] a')
-        datas_urls.push(data['href'])
+          puts row.text
       end
-    end
+   end
     return datas_urls
-  end
+ end
 end
