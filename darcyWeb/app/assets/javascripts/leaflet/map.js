9b2135f8bceac61086d1ea17d7bea31ebe81474e
Kairon Velozo <kairon333@gmail.com>
2017-05-14 11:36:04 -0300

    Fixing files for testing.

--- darcyWeb/app/assets/javascripts/leaflet/map.js
+++ darcyWeb/app/assets/javascripts/leaflet/map.js
@@ -9,14 +9,15 @@ const bounds = L.latLngBounds(south_west, north_east);
 var map = L.map('map', {
     maxBounds: bounds,
     center: centerMap,
-    zoom: 15,
+    zoom: 16,
     minZoom: 16,
     contextmenu: false,
-    contextmenuWidth: 140,
-
+    contextmenuWidth: 140
 });
 
-L.tileLayer('https://api.mapbox.com/styles/v1/kaironvzb/cj1m79xc9000p2sqrhrjdhel2/tiles/256/{z}/{x}/{y}?access_token=pk.eyJ1Ijoia2Fpcm9udnpiIiwiYSI6ImNpd21mbW0wbDAwNXMyenFpanlmbHZ6ZXAifQ.RtMAGQj_0ho54Rw6D812hw', {
+var url = "https://api.mapbox.com/styles/v1/kaironvzb/cj1y3dkki00042sn074lbuo5k/tiles/256/{z}/{x}/{y}?access_token=pk.eyJ1Ijoia2Fpcm9udnpiIiwiYSI6ImNpd21mbW0wbDAwNXMyenFpanlmbHZ6ZXAifQ.RtMAGQj_0ho54Rw6D812hw";
+
+L.tileLayer(url, {
     attribution: '&copy; <a href="https://www.mapbox.com/map-feedback/">Mapbox</a>\
                   &copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
 }).addTo(map);
