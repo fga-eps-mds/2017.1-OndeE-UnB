1079f30124b256b1466183637f8cbfcd966938c0
tayh <tayhcarvalho@gmail.com>
2017-05-20 19:09:45 +0000

    Creating department search method

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,9 +1,31 @@
 SearchController < MapController
 
-  def collect_department_data
+  def index
      geo_json = json_department_search(params[:search])
      render plain: geo_json.to_json
   end
 
+  def json_department_search(search)
+    @department = Department.where('acronym LIKE ?', "%#{search}%")
+    features = []
+    @department.each_with_index do |department, index|
 
+      properties = {
+        popupContent: "MDS",
+        title: department.acronym,
+        description: department.title,
+        image: 'fa-department'
+      }
+
+      geo_data = JSON.parse department.geo_data
+      geo_data['features'].each do |feature|
+        feature.merge!(properties: properties)
+        features.push(feature)
+      end
+
+    end
+    geo_json = {
+     type: "FeatureCollection",
+     features: features
+    }
 end
