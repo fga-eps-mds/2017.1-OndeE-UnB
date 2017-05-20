ceb2770d62f76cd82988c810e672be984a487d6c
tayh <tayhcarvalho@gmail.com>
2017-05-20 20:16:43 +0000

    Crating search route

--- config/routes.rb
+++ config/routes.rb
@@ -30,6 +30,7 @@ Rails.application.routes.draw do
   get 'map/building/:id', to: 'map#building'
 
   get 'map/routes'
+  get 'map/search', to: 'search#index'
   get 'map/search_building'
   get 'map/collect_building_data'
   get 'map/building'
