aa6edac7bebfa5af1af5f7208bff95f392f47bcf
Daniel <danmoura17@gmail.com>
2017-05-21 15:19:30 +0000

    Remaking admin routes

---
+++ app/controllers/admin/admins_controller.rb
@@ -0,0 +1,29 @@
+class Admin::AdminsController < AdminController
+  before_action :set_admin, only: [:destroy, :edit, :update]
+
+    def index
+        @admins = Admin.all
+    end
+
+    def new
+        @admins= Admin.new
+    end
+
+    def create
+        @admin = Admin.new(admin_params)
+        if @admin.save
+          redirect_to admin_adm_path, notice: alert_success('Administrador criado com êxito.')
+        else
+          render :new
+        end
+  end
+
+    def update
+    end
+
+
+
+    def destroy
+    end
+
+end
