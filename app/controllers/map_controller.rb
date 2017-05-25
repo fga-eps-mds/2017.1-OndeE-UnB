5ef3541fd1f13272d6bf081395613fe63cb701aa
Tayh <tayhcarvalho@gmail.com>
2017-05-25 15:08:29 -0300

    Deleting search method from map controller.

--- app/controllers/map_controller.rb
+++ app/controllers/map_controller.rb
@@ -16,18 +16,6 @@ class MapController < ApplicationController
   end
 
   def routes
-      @buildings = Building.all
   end
-
-  def search_building
-      @buildings = Building.all
-  end
-
-  def collect_building_data
-    geo_json = json_building_search
-    render plain: geo_json.to_json
-  end
-
-
 
 end
