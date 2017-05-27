89a8ce86a8cf289084a303a11a797547d3f3388f
alexandretk <alexandrekry@gmail.com>
2017-05-27 11:49:16 -0300

    Customizing icons for Departments

--- app/assets/javascripts/admin.js
+++ app/assets/javascripts/admin.js
@@ -1,8 +1,8 @@
 //= require admin/vendor.js
 //= require admin/app.js
 //= require admin/locations.js
-//= require admin/buildings.js
-//= require admin/points.js
+//= r equire admin/buildings.js
+//= r equire admin/points.js
 //= rails.validations
 
 $('#map').height($(document).height()-90);
