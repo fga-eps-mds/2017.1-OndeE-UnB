49837faea708f94dd757263fdda8f56fac30b6a9
alexandretk <alexandrekry@gmail.com>
2017-06-01 17:37:51 -0300

    Copying URL to clipboard and showing a point on the map when accessing the shared page

--- app/assets/javascripts/map/routes.js
+++ app/assets/javascripts/map/routes.js
@@ -261,6 +261,8 @@ function routesToHere(e) {
   control.spliceWaypoints(control.getWaypoints().length - 1, 1, e.latlng);
 }
 
+
+
 var sharedLocation = {
   marker: null,
   title: 'sharedLocation',
@@ -268,25 +270,58 @@ var sharedLocation = {
   color: 'blue'
 };
 
+
+function copyToClipboard(text) {
+    if (window.clipboardData && window.clipboardData.setData) {
+        // IE specific code path to prevent textarea being shown while dialog is visible.
+        return clipboardData.setData("Text", text); 
+
+    } else if (document.queryCommandSupported && document.queryCommandSupported("copy")) {
+        var textarea = document.createElement("textarea");
+        textarea.textContent = text;
+        textarea.style.position = "fixed";  // Prevent scrolling to bottom of page in MS Edge.
+        document.body.appendChild(textarea);
+        textarea.select();
+        try {
+            return document.execCommand("copy");  // Security exception may be thrown by some browsers.
+        } catch (ex) {
+            console.warn("Copy to clipboard failed.", ex);
+            return false;
+        } finally {
+            document.body.removeChild(textarea);
+        }
+    }
+}
+
+
+
 function setSharedLocation(e, waypoint) {
   var lat = e.latlng.lat;
   var lng = e.latlng.lng;
 
-  var data = {};
-  data[waypoint.title] = lat + ", " + lng;
+  //alert(e.latlng);
+
+  // var data = {};
+  // data[waypoint.title] = lat + ", " + lng;
 
   if (waypoint.marker == null) {
     createMarker(waypoint, e.latlng);
   } else {
     waypoint.marker.setLatLng(e.latlng);
   }
-  alert(e.latlng);
+  
+  var linkUrl = window.location.host;
+  linkUrl += "/findme?lat="+lat+"&lng="+lng;
+  //alert(linkUrl);
+  copyToClipboard(linkUrl);
+  alert("Link copiado, cole onde desejar e envie para seu colega!");
 };
 
 function shareLocation(e) {
   setSharedLocation(e, sharedLocation);
-  control.spliceWaypoints(control.getWaypoints().length - 1, 1, e.latlng);
 }
+
+
 
 
 function reverseRoute(e) {
--- config/routes.rb
+++ config/routes.rb
@@ -13,6 +13,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   root 'map#index'
+  get 'findme', to: 'map#index'
 
   namespace :admin do
     resources :buildings, except: [:show]
