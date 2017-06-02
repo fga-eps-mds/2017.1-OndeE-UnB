74908fe95aad75e138d962e7ba87e329ac834711
EduardoBrasil <brasil.eduardo1@gmail.com>
2017-06-02 12:23:24 -0300

    Creating tests for map filters.

--- spec/features/map_filter_spec.rb
+++ spec/features/map_filter_spec.rb
@@ -2,19 +2,66 @@ require 'rails_helper'
 
 describe "Map filter", type: :feature do
 
-	it "should show filter button", js: true do
-    #
+	before(:each) do
 		visit root_path
+	end
+
+	it "should show filter button", js: true do
     control = page.evaluate_script("$('.leaflet-control-layers').length")
 		expect(control).to be(1)
 	end
 
+	it "should toggle the control layer", js: true  do
+		page.find('a.leaflet-control-layers-toggle').click
+		expect(page).to have_content('Satélite')
+	end
+
   it "should hide buildings when building checkbox is unchecked", js: true do
-    visit root_path
     # clicks the buildings checkbox
     page.execute_script("$('.leaflet-control-layers-selector').eq(2).trigger('click')")
     layer = page.evaluate_script("map.hasLayer(buildingLayer)")
     expect(layer).to be(false)
   end
 
+	it "should show the bicycle racks when bicycle rack checkbox is checked", js: true do
+		page.execute_script("$('.leaflet-control-layers-selector').eq(3).trigger('click')")
+		layer = page.evaluate_script("map.hasLayer(bikesLayer)")
+		expect(layer).to be(true)
+	end
+
+	it "should show the bathrooms when bathrooms checkbox is checked", js: true do
+		page.execute_script("$('.leaflet-control-layers-selector').eq(4).trigger('click')")
+		layer = page.evaluate_script("map.hasLayer(bathroomLayer)")
+		expect(layer).to be(true)
+	end
+
+	it "should show the snackbar when snackbars checkbox is checked", js: true do
+		page.execute_script("$('.leaflet-control-layers-selector').eq(5).trigger('click')")
+		layer = page.evaluate_script("map.hasLayer(snackbarLayer)")
+		expect(layer).to be(true)
+	end
+
+	it "should show the entrance when the entrances checkbox is checked", js: true do
+		page.execute_script("$('.leaflet-control-layers-selector').eq(6).trigger('click')")
+		layer = page.evaluate_script("map.hasLayer(entranceLayer)")
+		expect(layer).to be(true)
+	end
+
+	it "should show the busstop when the busstops checkbox is checked", js: true do
+		page.execute_script("$('.leaflet-control-layers-selector').eq(7).trigger('click')")
+		layer = page.evaluate_script("map.hasLayer(busstopLayer)")
+		expect(layer).to be(true)
+	end
+
+	it "should show the satelite map when the satelite button is selected", js: true do
+		page.execute_script("$('.leaflet-control-layers-selector').eq(0).trigger('click')")
+		layer = page.evaluate_script("map.hasLayer(esri_WorldImagery)")
+		expect(layer).to be(true)
+	end
+
+	it "should not show the street map when the streets button is not selected", js: true do
+		page.execute_script("$('.leaflet-control-layers-selector').eq(0).trigger('click')")
+		layer = page.evaluate_script("map.hasLayer(mapBox)")
+		expect(layer).to be(false)
+	end
 end
