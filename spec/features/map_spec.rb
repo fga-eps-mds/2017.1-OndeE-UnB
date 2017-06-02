fdd53b5228a4ff8e776d18f60d8e66ecf870a720
EduardoBrasil <brasil.eduardo1@gmail.com>
2017-06-02 12:24:22 -0300

    Creating some Points and recreating a test.

--- spec/features/map_spec.rb
+++ spec/features/map_spec.rb
@@ -1,28 +1,25 @@
 require 'rails_helper'
 
 describe 'Map', type: :feature do
+
   it 'should load the map', js: true do
-    visit root_path
     map = page.evaluate_script("$('.leaflet-container').length")
     expect(map).to eq(1)
   end
 
-  it 'should map zoom be at 16', js: true do
-    visit root_path
+  it 'should map zoom be at 17', js: true do
     zoom = page.evaluate_script('map.getZoom()')
-    expect(zoom).to eq(16)
+    expect(zoom).to eq(17)
   end
 
   it 'should not show the sidebar when you open the map', js: true do
-    visit root_path
     sidebar = page.evaluate_script("$('#sidebar').html()")
     expect(sidebar).to be_empty
   end
 
   it 'should show the sidebar when click on building', js: true do
-    visit root_path
     page.execute_script("buildingLayer.getLayers()[0].fire('click')")
-		wait_for_ajax
+	  wait_for_ajax
     sidebar = page.evaluate_script('sidebar.isVisible()')
     expect(sidebar).to eq(true)
   end
