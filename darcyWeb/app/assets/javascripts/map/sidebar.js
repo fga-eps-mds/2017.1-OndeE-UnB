4a93fe3d36e0324bd740017a0eae11df463b1d79
Mateus Vieira <mateusvroriz6b@gmail.com>
2017-06-02 16:01:27 -0300

    Fixing the sidebar slider for mobile devices

--- darcyWeb/app/assets/javascripts/map/sidebar.js
+++ darcyWeb/app/assets/javascripts/map/sidebar.js
@@ -7,3 +7,33 @@ var sidebar = L.control.sidebar('sidebar', {
 }).addTo(map);
 
 var visible = sidebar.isVisible();
+sidebar.on('shown', function () {
+  var myBlock = document.getElementsByClassName('sidebar-header')[0];
+
+  var mc = new Hammer(myBlock);
+
+  var pan = new Hammer.Pan({ direction: Hammer.DIRECTION_ALL, threshold: 0 });
+
+  var sidebarEl = $('#sidebar');
+
+  mc.add(pan);
+
+  mc.on("pan", handleDrag);
+
+  var lastPosY = 0;
+  function handleDrag(ev) {
+
+    console.log('okkk');
+
+    var elem = ev.target;
+
+    lastPosY = sidebarEl.css('top');
+    console.log(parseInt(lastPosY));
+
+    var posY = ev.deltaY + lastPosY;
+
+    sidebarEl.css('top', posY + "px");
+    sidebarEl.height('50px');
+  }
+
+});
