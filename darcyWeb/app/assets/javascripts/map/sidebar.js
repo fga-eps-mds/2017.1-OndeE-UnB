635059304441ec5e539dcbcdf49ccb311a3c1134
Stéfane Souza <stefanesouza04@gmail.com>
2017-06-03 02:16:23 -0300

    Adding responsive screens.

--- darcyWeb/app/assets/javascripts/map/sidebar.js
+++ darcyWeb/app/assets/javascripts/map/sidebar.js
@@ -6,34 +6,35 @@ var sidebar = L.control.sidebar('sidebar', {
     closeButton: false
 }).addTo(map);
 
-var visible = sidebar.isVisible();
-sidebar.on('shown', function () {
-  var myBlock = document.getElementsByClassName('sidebar-header')[0];
+//var visible = sidebar.isVisible();
 
-  var mc = new Hammer(myBlock);
-
-  var pan = new Hammer.Pan({ direction: Hammer.DIRECTION_ALL, threshold: 0 });
-
-  var sidebarEl = $('#sidebar');
-
-  mc.add(pan);
-
-  mc.on("pan", handleDrag);
-
-  var lastPosY = 0;
-  function handleDrag(ev) {
-
-    console.log('okkk');
-
-    var elem = ev.target;
-
-    lastPosY = sidebarEl.css('top');
-    console.log(parseInt(lastPosY));
-
-    var posY = ev.deltaY + lastPosY;
-
-    sidebarEl.css('top', posY + "px");
-    sidebarEl.height('50px');
-  }
-
-});
+// sidebar.on('shown', function () {
+//   var myBlock = document.getElementsByClassName('sidebar-header')[0];
+//
+//   var mc = new Hammer(myBlock);
+//
+//   var pan = new Hammer.Pan({ direction: Hammer.DIRECTION_ALL, threshold: 0 });
+//
+//   var sidebarEl = $('#sidebar');
+//
+//   mc.add(pan);
+//
+//   mc.on("pan", handleDrag);
+//
+//   var lastPosY = 0;
+//   function handleDrag(ev) {
+//
+//     console.log('okkk');
+//
+//     var elem = ev.target;
+//
+//     lastPosY = sidebarEl.css('top');
+//     console.log(parseInt(lastPosY));
+//
+//     var posY = ev.deltaY + lastPosY;
+//
+//     sidebarEl.css('top', posY + "px");
+//     sidebarEl.height('50px');
+//   }
+//
+// });
