db6fbb5115cd455e93d4662bbea073109736eddd
Kairon Velozo <kairon333@gmail.com>
2017-06-04 14:30:03 -0300

    Refactoring drawing javascripts files.

--- app/assets/javascripts/admin/rooms.js
+++ app/assets/javascripts/admin/rooms.js
@@ -1,79 +1,60 @@
-//= require leaflet/draw
-//= require leaflet/map
-//= require leaflet/easy-button
-//= require leaflet/draw.translations
+// require ./plants
 
-const $building_geo_data = {
-  element: $('#building_geo_data'),
-  save: function(geo_json){
-    this.element.val(JSON.stringify(geo_json.toGeoJSON()));
-  },
-  load: function(){
-    const $geo_json = this.element.val();
-    if ($geo_json) {
-        drawnLayer.addData(JSON.parse($geo_json));
-    }
-  }
-};
+$('.rooms.new, .rooms.edit').ready(function() {
 
-const $building_coords = {
-  element_lat: $('#building_latitude'),
-  element_lng: $('#building_longitude'),
-  save: function(lat, lng){
-    if (this.element_lat.val(lat) && this.element_lng.val(lng)) {
-      return true;
-    } else {
-    return false; }
-  },
-  load: function(){
-    const $lat = this.element_lat.val();
-    const $lng = this.element_lng.val();
-    if ($lat && $lng) {
-        map.flyTo(new L.LatLng($lat, $lng));
-    }
-  }
-}
-$building_coords.load();
-
-var drawnLayer = L.geoJSON().addTo(map);
-map.addLayer(drawnLayer);
-
-// Load json from the form
-$building_geo_data.load();
-
-var drawControl = new L.Control.Draw({
+  var drawControlRooms = new L.Control.Draw({
     edit: {
-        featureGroup: drawnLayer
+      featureGroup: drawnLayer
     },
+
     draw: {
-        polyline: false,
-        circle: false,
-        rectangle: false,
-        marker: false,
-        polygon: {
-            allowIntersection: false,
-            showArea: true
-        }
+      polyline: false,
+      circle: false,
+      rectangle: false,
+      marker: false,
+      polygon: {
+        allowIntersection: false,
+        showArea: true
+      }
     }
+  });
+
+  map.on(L.Draw.Event.CREATED, function(event) {
+    disableDrawing();
+
+    const centerPoint = drawnLayer.getLayers()[0].getBounds().getCenter();
+    console.log(centerPoint);
+    // locationCoods.save(centerPoint.lat, centerPoint.lng);
+    // $point_geo_data.save(drawnLayerPoints);
+  });
+
+  map.on(L.Draw.Event.DELETED, function(event) {
+
+  });
+
+  map.on(L.Draw.Event.DELETED, function(event) {
+    enableDrawing();
+  });
+
+  map.addControl(drawControlRooms);
+
+  function enableDrawing(){
+    // enable draw polygon
+    drawControlRooms.setDrawingOptions({
+      polygon: true
+    });
+    map.removeControl(drawControlRooms);
+    map.addControl(drawControlRooms);
+  }
+
+  function disableDrawing(){
+    // remove the draw control from map
+    map.removeControl(drawControlRooms);
+    // set polygon as false
+    drawControlRooms.setDrawingOptions({
+      polygon: false
+    });
+    // add the draw control with the draw polygon disabled
+    map.addControl(drawControlRooms);
+  }
 });
-
-map.on(L.Draw.Event.CREATED, function(event) {
-    var layer = event.layer;
-    drawnLayer.addLayer(layer);
-    $building_geo_data.save(drawnLayer);
-});
-
-map.on(L.Draw.Event.EDITED, function(event) {
-    $building_geo_data.save(drawnLayer);
-});
-
-map.on(L.Draw.Event.DELETED, function(event) {
-    $building_geo_data.save(drawnLayer);
-});
-
-map.addControl(drawControl);
-
-L.easyButton('fa-map-marker', function(btn, map){
-  const $center = map.getCenter();
-  $building_coords.save($center.lat, $center.lng);
-}).addTo(map);
