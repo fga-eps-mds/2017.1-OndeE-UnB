3d9a35124f837eee3fd5bf63b9eaa6f9308972f5
Kairon Velozo <kairon333@gmail.com>
2017-06-04 14:30:03 -0300

    Refactoring drawing javascripts files.

--- darcyWeb/app/assets/javascripts/admin.js
+++ darcyWeb/app/assets/javascripts/admin.js
@@ -1,9 +1,12 @@
 //= require jquery
 //= require jquery_ujs
+//= require jquery/ready-selector
 
 //= require admin/vendor.js
 //= require admin/app.js
 //= require admin/locations.js
+//= require admin/rooms.js
+
 //= rails.validations
 
 $('#map').height($(document).height()-90);
--- darcyWeb/app/assets/javascripts/admin/locations.js
+++ darcyWeb/app/assets/javascripts/admin/locations.js
@@ -3,74 +3,74 @@
 //= require leaflet/easy-button
 //= require leaflet/draw.translations
 
-const $location_geo_data = {
+const locationGeoData = {
   element: $("input[id$='_geo_data']"),
   save: function(geo_json){
     this.element.val(JSON.stringify(geo_json.toGeoJSON()));
   },
   load: function(){
-    const $geo_json = this.element.val();
-    if ($geo_json) {
-        drawnLayer.addData(JSON.parse($geo_json));
+    const geoJSON = this.element.val();
+    if (geoJSON) {
+      drawnLayer.addData(JSON.parse(geoJSON));
     }
   }
 };
 
-const $location_coods = {
-  element_lat: $("input[id$='_latitude']"),
-  element_lng: $("input[id$='_longitude']"),
+const locationCoods = {
+  elementLat: $("input[id$='_latitude']"),
+  elementLng: $("input[id$='_longitude']"),
   save: function(lat, lng){
-    this.element_lat.val(lat);
-    this.element_lng.val(lng);
+    this.elementLat.val(lat);
+    this.elementLng.val(lng);
   },
   load: function(){
-    const $lat = this.element_lat.val();
-    const $lng = this.element_lng.val();
-    if ($lat && $lng) {
-        map.flyTo(new L.LatLng($lat, $lng));
+    const lat = this.elementLat.val();
+    const lng = this.elementLng.val();
+    if (lat && lng) {
+        map.flyTo(new L.LatLng(lat, lng));
     }
   }
 }
-$location_coods.load();
+locationCoods.load();
 
 
 var drawnLayer = L.geoJSON().addTo(map);
 map.addLayer(drawnLayer);
 
 // Load json from the form
-$location_geo_data.load();
+locationGeoData.load();
 
-var drawControl = new L.Control.Draw({
-    edit: {
-        featureGroup: drawnLayer
-    },
-    draw: {
-        polyline: false,
-        circle: false,
-        rectangle: false,
-        polygon: {
-            allowIntersection: false,
-            showArea: true
-        }
-    }
-});
+// var drawControl = new L.Control.Draw({
+//     edit: {
+//         featureGroup: drawnLayer
+//     },
+//     draw: {
+//         polyline: false,
+//         circle: false,
+//         rectangle: false,
+//         polygon: {
+//             allowIntersection: false,
+//             showArea: true
+//         }
+//     }
+// });
 
 map.on(L.Draw.Event.CREATED, function(event) {
     var layer = event.layer;
     drawnLayer.addLayer(layer);
-    $location_geo_data.save(drawnLayer);
+    locationGeoData.save(drawnLayer);
 });
 
 map.on(L.Draw.Event.EDITED, function(event) {
-    $location_geo_data.save(drawnLayer);
+    locationGeoData.save(drawnLayer);
 });
 map.on(L.Draw.Event.DELETED, function(event) {
-    $location_geo_data.save(drawnLayer);
+    locationGeoData.save(drawnLayer);
 });
 
 map.addControl(drawControl);
 
 L.easyButton('fa-map-marker', function(btn, map){
   const $center = map.getCenter();
-  $location_coods.save($center.lat, $center.lng);
+  locationCoods.save($center.lat, $center.lng);
 }).addTo(map);
