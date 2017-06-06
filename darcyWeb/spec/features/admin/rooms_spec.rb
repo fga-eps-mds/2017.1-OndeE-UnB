d2d0187d07e565b532e738b5a4434cb1ea525b08
Matheus Mello <mat.mello93@gmail.com>
2017-06-06 15:10:55 -0300

    room before tests

--- darcyWeb/spec/features/admin/rooms_spec.rb
+++ darcyWeb/spec/features/admin/rooms_spec.rb
@@ -1,1 +1,19 @@
 require 'rails_helper'
+
+describe 'Room', type: :feature do
+
+      context "New room" do
+
+            email = "admin@admin.com"
+            senha = "123456"
+            before(:each) do
+                  Capybara.raise_server_errors = false
+                  visit admin_session_path
+                  fill_in 'admin[email]', with: email
+                  fill_in 'admin[password]', with: senha
+                  find('input.btn.btn-success.btn-submit').click
+                  visit new_admin_point_path
+            end
+
+      end
+end
