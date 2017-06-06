f0f59dd4d3c63f010430c5294f4a8738f8c312af
Matheus Nascimento <matheus@iMac-de-Matheus.local>
2017-06-06 18:57:59 -0300

    room tests

--- darcyWeb/spec/features/admin/rooms_spec.rb
+++ darcyWeb/spec/features/admin/rooms_spec.rb
@@ -136,4 +136,96 @@ describe 'Rooms', type: :feature do
       expect(page).to have_current_path(new_admin_room_path)
     end
 
+    it "Should Not save room if has not longitude", js: true  do
+      FactoryGirl.create :building
+      room = FactoryGirl.build :room
+
+      scpt = page.evaluate_script("$('#room_geo_data').val()")
+      puts scpt
+
+      find('#map').click
+      within("form") do
+        fill_in 'room[acronym]', with: room.acronym
+        fill_in 'room[title]', with: room.title
+        select 'BSA', from: :room_building_id
+        select 'Laboratório', from: :room_room_type
+        fill_in 'room[level]', with: room.level
+        fill_in 'room[latitude]', with: room.latitude
+        fill_in 'room[longitude]', with: ''
+        fill_in 'room[geo_data]', with: room.geo_data
+        page.save_screenshot
+        page.execute_script("$('form').submit()")
+      end
+      expect(page).to have_current_path(new_admin_room_path)
+    end
+
+    it "Should Not save room if has not GeoData", js: true  do
+      FactoryGirl.create :building
+      room = FactoryGirl.build :room
+
+      scpt = page.evaluate_script("$('#room_geo_data').val()")
+      puts scpt
+
+      find('#map').click
+      within("form") do
+        fill_in 'room[acronym]', with: room.acronym
+        fill_in 'room[title]', with: room.title
+        select 'BSA', from: :room_building_id
+        select 'Laboratório', from: :room_room_type
+        fill_in 'room[level]', with: room.level
+        fill_in 'room[latitude]', with: room.latitude
+        fill_in 'room[longitude]', with: room.longitude
+        fill_in 'room[geo_data]', with: ''
+        page.save_screenshot
+        page.execute_script("$('form').submit()")
+      end
+      expect(page).to have_current_path(new_admin_room_path)
+    end
+
+    it "Should Not save room without related building", js: true  do
+      FactoryGirl.create :building
+      room = FactoryGirl.build :room
+
+      scpt = page.evaluate_script("$('#room_geo_data').val()")
+      puts scpt
+
+      find('#map').click
+      within("form") do
+        fill_in 'room[acronym]', with: room.acronym
+        fill_in 'room[title]', with: room.title
+        select 'BSA', from: ''
+        select 'Laboratório', from: :room_room_type
+        fill_in 'room[level]', with: room.level
+        fill_in 'room[latitude]', with: room.latitude
+        fill_in 'room[longitude]', with: room.longitude
+        fill_in 'room[geo_data]', with: room.geo_data
+        page.save_screenshot
+        page.execute_script("$('form').submit()")
+      end
+      expect(page).to have_current_path(new_admin_room_path)
+    end
+
+    it "Should Not save room without room type", js: true  do
+      FactoryGirl.create :building
+      room = FactoryGirl.build :room
+
+      scpt = page.evaluate_script("$('#room_geo_data').val()")
+      puts scpt
+
+      find('#map').click
+      within("form") do
+        fill_in 'room[acronym]', with: room.acronym
+        fill_in 'room[title]', with: room.title
+        select 'BSA', from: :room_building_id
+        select 'Laboratório', from: ''
+        fill_in 'room[level]', with: room.level
+        fill_in 'room[latitude]', with: room.latitude
+        fill_in 'room[longitude]', with: room.longitude
+        fill_in 'room[geo_data]', with: room.geo_data
+        page.save_screenshot
+        page.execute_script("$('form').submit()")
+      end
+      expect(page).to have_current_path(new_admin_room_path)
+    end
+
   end
