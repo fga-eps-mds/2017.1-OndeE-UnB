a9eae928e192519ea0cb8e1f15f6b3f3a0e915ba
Kairon Velozo <kairon333@gmail.com>
2017-06-06 22:31:32 -0300

    Fixing admin acceptance tests.

--- spec/factories.rb
+++ spec/factories.rb
@@ -10,8 +10,14 @@ FactoryGirl.define do
   end
 
   factory :point do
-
+    description "Descricao"
+    geo_data '{"type":"FeatureCollection","features":[{"type":"Feature","properties":{},"geometry":{"type":"Polygon","coordinates":[[[-47.867375314235694,-15.762756116441665],[-47.86702126264573,-15.762497981882012],[-47.866597473621376,-15.763055552119592],[-47.8669622540474,-15.763303360622453],[-47.867375314235694,-15.762756116441665]]]}}]}'
+    latitude 0.0
+    longitude 0.0
+    title "Bicicletario"
+    type_point "Bicicletario"
   end
+
 
   factory :building do
     acronym "BSA"
