4c03ccd42a8fdaca8b97d02e038b177204ec885c
Stéfane Souza <stefanesouza04@gmail.com>
2017-06-09 01:12:21 -0300

    Translating more routes in to walk mode.

--- app/assets/javascripts/map/routes/translations.js
+++ app/assets/javascripts/map/routes/translations.js
@@ -14,4 +14,38 @@ const routesTranslations = {
   "Walk west.": "Ande para o oeste."
   "Walk south.": "Ande para o sul."
   "Walk north.": "Ande para o norte."
+
+  "Continue.": "Continue."
+  "Turn left.": "Vire à esquerda."
+  "Turn right.": "Vire à direita."
+  "Turn north.": "Vire para o norte."
+  "Turn south.": "Vire para o sul."
+
+  "Bear right.": "Fique à direita."
+  "Bear left.": "Fique à esquerda."
+  "Bear north.": "Fique ao norte."
+  "Bear south.": "Fique ao sul."
+
+  "Continue on the walkway.": "Continue na calçada."
+  "Turn right onto the walkway.": "Vire à direta na calçada."
+  "Turn left onto the walkway.": "Vire à esquerda na calçada."
+  "Walk east on the walkway.": "Ande para o leste na calçada."
+  "Walk south on the walkway.": "Ande para o sul na calçada."
+  "Walk west on the walkway.": "Ande para o oeste na calçada."
+  "Walk north on the walkway.": "Ande para o norte na calçada."
+
+  "Bear left onto the walkway.": "Fique à esquerda na calçada."
+  "Bear right onto the walkway.": "Fique à direta na calçada."
+  "Bear north onto the walkway.": "Fique ao norte na calçada."
+  "Bear south onto the walkway.": "Fique ao sul na calçada."
+
+  "Enter the roundabout and take the 1st exit.": "Entre na rotatória e pegue a 1ª saída."
+  "Enter the roundabout and take the 2nd exit.": "Entre na rotatória e pegue a 2ª saída."
+  "Exit the roundabout.": "Saia da rotatória."
+
+  "Make a left U-turn.": "Faça uma curva U para à esquerda."
+  "Make a right U-turn.": "Faça uma curva U para à direita."
+
+  // "Turn sharp right onto the walkway.": ""
+  // "Turn sharp right.": ""
 };
