0f09cee11a21b5d7dadfd534692e3fda546a42af
Lucas Souza <lucas.soaresouza@gmail.com>
2017-06-09 00:14:57 +0000

    Validates in courses class

--- app/models/course.rb
+++ app/models/course.rb
@@ -1,3 +1,15 @@
 class Course < ApplicationRecord
   belongs_to :room
+
+  enum day_of_week: [:sunday, :monday, :tuesday, :wednesday, :thursday, :friday, :saturday]
+  validates :title, presence: true
+  validates :code, presence: true
+  validates :day_of_week, inclusion: {in: day_of_weeks.keys }
+  validates :start_time, presence: true
+  validates :end_time, presence: true
+  validates :class, presence: true
+
+  translate_enum :day_of_week
+
+
 end
