821197f12d1cd767bd847445a8503f8d09751014
Kairon Velozo <kairon333@gmail.com>
2017-06-08 21:25:14 -0300

    [TDD] Setting up course model tests.

--- spec/models/course_spec.rb
+++ spec/models/course_spec.rb
@@ -8,33 +8,32 @@ RSpec.describe Course, type: :model do
     end
 
     it "should be invalid when code is blank" do
-
+        course = FactoryGirl.build :course, code: ''
         expect(course).to be_invalid
     end
 
     it "should be invalid when title is blank" do
-
+        course = FactoryGirl.build :course, title: ''
         expect(course).to be_invalid
     end
 
     it "should be invalid when day_of_week is blank" do
-
+        course = FactoryGirl.build :course, day_of_week: ''
         expect(course).to be_invalid
     end
 
     it "should be invalid when start_time data is blank" do
-
+        course = FactoryGirl.build :course, start_time: ''
         expect(course).to be_invalid
     end
 
     it "should be invalid when end_time is blank" do
-
+        course = FactoryGirl.build :course, end_time: ''
         expect(course).to be_invalid
     end
 
-    it "should be invalid when class is blank" do
-
+    it "should be invalid when classroom is blank" do
+        course = FactoryGirl.build :course, classroom: ''
         expect(course).to be_invalid
     end
 end
-
