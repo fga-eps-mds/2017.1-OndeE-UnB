b838194ffa87cb96de3e658a88ad6b23c19f3d89
Stéfane Souza <stefanesouza04@gmail.com>
2017-06-09 14:07:22 -0300

    Solving problem with translations.

--- darcyWeb/app/assets/javascripts/map/routes/translations.js
+++ darcyWeb/app/assets/javascripts/map/routes/translations.js
@@ -1,96 +1,95 @@
 const routesTranslations = {
   // Routes for to walk mode
-  "You have arrived at your destination.": "Você chegou ao seu destino."
-  "Your destination is on the left.": "Seu destino está à esquerda."
-  "Your destination is on the right.": "Seu destino está à direita."
-  "Your destination is on the north.": "Seu destino está ao norte."
-  "Your destination is on the south.": "Seu destino está ao sul."
+  "You have arrived at your destination.": "Você chegou ao seu destino.",
+  "Your destination is on the left.": "Seu destino está à esquerda.",  "Your destination is on the right.": "Seu destino está à direita.",
+  "Your destination is on the north.": "Seu destino está ao norte.",
+  "Your destination is on the south.": "Seu destino está ao sul.",
 
-  "Walk southeast.": "Ande em direção sudeste."
-  "Walk northeast.": "Ande em direção nordeste."
-  "Walk northwest.": "Ande em direção noroeste."
-  "Walk southwest.": "Ande em direção sudoeste."
-  "Walk east.": "Ande para o leste."
-  "Walk west.": "Ande para o oeste."
-  "Walk south.": "Ande para o sul."
-  "Walk north.": "Ande para o norte."
+  "Walk southeast.": "Ande em direção sudeste.",
+  "Walk northeast.": "Ande em direção nordeste.",
+  "Walk northwest.": "Ande em direção noroeste.",
+  "Walk southwest.": "Ande em direção sudoeste.",
+  "Walk east.": "Ande para o leste.",
+  "Walk west.": "Ande para o oeste.",
+  "Walk south.": "Ande para o sul.",
+  "Walk north.": "Ande para o norte.",
 
-  "Continue.": "Continue."
-  "Turn left.": "Vire à esquerda."
-  "Turn right.": "Vire à direita."
-  "Turn north.": "Vire para o norte."
-  "Turn south.": "Vire para o sul."
+  "Continue.": "Continue.",
+  "Turn left.": "Vire à esquerda.",
+  "Turn right.": "Vire à direita.",
+  "Turn north.": "Vire para o norte.",
+  "Turn south.": "Vire para o sul.",
 
-  "Bear right.": "Fique à direita."
-  "Bear left.": "Fique à esquerda."
-  "Bear north.": "Fique ao norte."
-  "Bear south.": "Fique ao sul."
+  "Bear right.": "Fique à direita.",
+  "Bear left.": "Fique à esquerda.",
+  "Bear north.": "Fique ao norte.",
+  "Bear south.": "Fique ao sul.",
 
-  "Continue on the walkway.": "Continue na calçada."
-  "Turn right onto the walkway.": "Vire à direta na calçada."
-  "Turn left onto the walkway.": "Vire à esquerda na calçada."
-  "Walk east on the walkway.": "Ande para o leste na calçada."
-  "Walk south on the walkway.": "Ande para o sul na calçada."
-  "Walk west on the walkway.": "Ande para o oeste na calçada."
-  "Walk northeast on the walkway.": "Ande para o nordeste na calçada."
-  "Walk northwest on the walkway.": "Ande para o noroeste na calçada."
-  "Walk southeast on the walkway.": "Ande para o sudeste na calçada."
-  "Walk southwest on the walkway.": "Ande para o sudoeste na calçada."
+  "Continue on the walkway.": "Continue na calçada.",
+  "Turn right onto the walkway.": "Vire à direta na calçada.",
+  "Turn left onto the walkway.": "Vire à esquerda na calçada.",
+  "Walk east on the walkway.": "Ande para o leste na calçada.",
+  "Walk south on the walkway.": "Ande para o sul na calçada.",
+  "Walk west on the walkway.": "Ande para o oeste na calçada.",
+  "Walk northeast on the walkway.": "Ande para o nordeste na calçada.",
+  "Walk northwest on the walkway.": "Ande para o noroeste na calçada.",
+  "Walk southeast on the walkway.": "Ande para o sudeste na calçada.",
+  "Walk southwest on the walkway.": "Ande para o sudoeste na calçada.",
 
-  "Bear left onto the walkway.": "Fique à esquerda na calçada."
-  "Bear right onto the walkway.": "Fique à direta na calçada."
-  "Bear north onto the walkway.": "Fique ao norte na calçada."
-  "Bear south onto the walkway.": "Fique ao sul na calçada."
+  "Bear left onto the walkway.": "Fique à esquerda na calçada.",
+  "Bear right onto the walkway.": "Fique à direta na calçada.",
+  "Bear north onto the walkway.": "Fique ao norte na calçada.",
+  "Bear south onto the walkway.": "Fique ao sul na calçada.",
 
-  "Enter the roundabout and take the 1st exit.": "Entre na rotatória e pegue a 1ª saída."
-  "Enter the roundabout and take the 2nd exit.": "Entre na rotatória e pegue a 2ª saída."
-  "Exit the roundabout.": "Saia da rotatória."
+  "Enter the roundabout and take the 1st exit.": "Entre na rotatória e pegue a 1ª saída.",
+  "Enter the roundabout and take the 2nd exit.": "Entre na rotatória e pegue a 2ª saída.",
+  "Exit the roundabout.": "Saia da rotatória.",
 
-  "Make a left U-turn.": "Faça uma curva U para à esquerda."
-  "Make a right U-turn.": "Faça uma curva U para à direita."
+  "Make a left U-turn.": "Faça uma curva U para à esquerda.",
+  "Make a right U-turn.": "Faça uma curva U para à direita.",
 
-  "Turn sharp right onto the walkway.": "Vire a curva à direita na calçada."
-  "Turn sharp left onto the walkway.": "Vire a curva à esquerda na calçada."
-  "Turn sharp right.": "Vire a curva à direita."
-  "Turn sharp left.": "Vire a curva à esquerda."
+  "Turn sharp right onto the walkway.": "Vire a curva à direita na calçada.",
+  "Turn sharp left onto the walkway.": "Vire a curva à esquerda na calçada.",
+  "Turn sharp right.": "Vire a curva à direita.",
+  "Turn sharp left.": "Vire a curva à esquerda.",
 
   // Routes for by car mode
-  "Drive east.": "Siga em direção leste."
-  "Drive west.": "Siga em direção oeste."
-  "Drive right.": "Siga à direita."
-  "Drive left.": "Siga à esquerda."
-  "Drive north.": "Siga em direção norte."
-  "Drive south.": "Siga em direção sul."
-  "Drive southeast.": "Siga em direção sudeste."
-  "Drive northeast.": "Siga em direção nordeste."
-  "Drive northwest.": "Siga em direção noroeste."
-  "Drive southwest.": "Siga em direção sudeste."
+  "Drive east.": "Siga em direção leste.",
+  "Drive west.": "Siga em direção oeste.",
+  "Drive right.": "Siga à direita.",
+  "Drive left.": "Siga à esquerda.",
+  "Drive north.": "Siga em direção norte.",
+  "Drive south.": "Siga em direção sul.",
+  "Drive southeast.": "Siga em direção sudeste.",
+  "Drive northeast.": "Siga em direção nordeste.",
+  "Drive northwest.": "Siga em direção noroeste.",
+  "Drive southwest.": "Siga em direção sudeste.",
 
   // Routes for by bike mode
-  "Bike east.": "Pedale em direção leste."
-  "Bike west.": "Pedale em direção oeste."
-  "Bike right.": "Pedale à direita."
-  "Bike left.": "Pedale à esquerda."
-  "Bike north.": "Pedale em direção norte."
-  "Bike south.": "Pedale em direção sul."
-  "Bike southeast.": "Pedale em direção sudeste."
-  "Bike northeast.": "Pedale em direção nordeste."
-  "Bike northwest.": "Pedale em direção noroeste."
-  "Bike southwest.": "Pedale em direção sudeste."
+  "Bike east.": "Pedale em direção leste.",
+  "Bike west.": "Pedale em direção oeste.",
+  "Bike right.": "Pedale à direita.",
+  "Bike left.": "Pedale à esquerda.",
+  "Bike north.": "Pedale em direção norte.",
+  "Bike south.": "Pedale em direção sul.",
+  "Bike southeast.": "Pedale em direção sudeste.",
+  "Bike northeast.": "Pedale em direção nordeste.",
+  "Bike northwest.": "Pedale em direção noroeste.",
+  "Bike southwest.": "Pedale em direção sudeste.",
 
-  "Turn left onto the cycleway.": "Vire à esquerda na ciclovia."
-  "Turn right onto the cycleway.": "Vire à direita na ciclovia."
-  "Turn north onto the cycleway.": "Vire ao norte na ciclovia."
-  "Turn south onto the cycleway.": "Vire ao sul na ciclovia."
+  "Turn left onto the cycleway.": "Vire à esquerda na ciclovia.",
+  "Turn right onto the cycleway.": "Vire à direita na ciclovia.",
+  "Turn north onto the cycleway.": "Vire ao norte na ciclovia.",
+  "Turn south onto the cycleway.": "Vire ao sul na ciclovia.",
 
-  "Bike east on the cycleway.": "Pedale em direção leste na ciclovia."
-  "Bike west on the cycleway.": "Pedale em direção oeste na ciclovia."
-  "Bike right on the cycleway.": "Pedale à direita na ciclovia."
-  "Bike left on the cycleway.": "Pedale à esquerda na ciclovia."
-  "Bike north on the cycleway.": "Pedale em direção norte na ciclovia."
-  "Bike south on the cycleway.": "Pedale em direção sul na ciclovia."
-  "Bike southeast on the cycleway.": "Pedale em direção sudeste na ciclovia."
-  "Bike northeast on the cycleway.": "Pedale em direção nordeste na ciclovia."
-  "Bike southwest on the cycleway.": "Pedale em direção sudeste na ciclovia."
-  "Bike northwest on the cycleway.": "Pedale em direção noroeste na ciclovia."
+  "Bike east on the cycleway.": "Pedale em direção leste na ciclovia.",
+  "Bike west on the cycleway.": "Pedale em direção oeste na ciclovia.",
+  "Bike right on the cycleway.": "Pedale à direita na ciclovia.",
+  "Bike left on the cycleway.": "Pedale à esquerda na ciclovia.",
+  "Bike north on the cycleway.": "Pedale em direção norte na ciclovia.",
+  "Bike south on the cycleway.": "Pedale em direção sul na ciclovia.",
+  "Bike southeast on the cycleway.": "Pedale em direção sudeste na ciclovia.",
+  "Bike northeast on the cycleway.": "Pedale em direção nordeste na ciclovia.",
+  "Bike southwest on the cycleway.": "Pedale em direção sudeste na ciclovia.",
+  "Bike northwest on the cycleway.": "Pedale em direção noroeste na ciclovia.",
 };
