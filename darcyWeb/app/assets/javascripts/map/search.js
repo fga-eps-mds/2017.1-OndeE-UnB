a676be2376e59c85e17162c5fb4a0074678c946a
Kairon Velozo <kairon333@gmail.com>
2017-06-09 22:53:04 -0300

    Setting auto complete for routing form.

--- darcyWeb/app/assets/javascripts/map/search.js
+++ darcyWeb/app/assets/javascripts/map/search.js
@@ -7,8 +7,28 @@ $(document).ready(function() {
   };
   $("#searchContainer").GeoJsonAutocomplete(options);
 
-  //sidebar.on('shown', function(){
-  //	$('.input-location').GeoJsonAutocomplete(options);
-  //	$('.searchButton, .divider, .clearButton').remove();
-  //})
+  sidebar.on('show', function() {
+    $('#route_origin, #route_destination').hide();
+    $('.input-location').GeoJsonAutocomplete(options);
+
+    $('.searchButton, .divider, .clearButton').remove();
+
+    $('.input-location').each(function(index, input){
+      $(input).on('DOMSubtreeModified', function() {
+        $(this).find('li').on('mouseenter', function() {
+            var latLng = L.latLng($(this).data('latitude'), $(this).data('longitude'));
+            latLng['latlng'] = {
+              lat: latLng.lat,
+              lng: latLng.lng
+            };
+            if($(input).hasClass('origin')){
+              routesFromHere(latLng);
+            } else {
+              routesToHere(latLng);
+            }
+
+        });
+      });
+    });
+  });
 });
