46507d4120a88c2fa6fde8c328de0598070a944e
Tayh <tayhcarvalho@gmail.com>
2017-06-10 11:07:56 -0300

    Creating tutorial button.

--- darcyWeb/app/assets/javascripts/map/tutorial.js
+++ darcyWeb/app/assets/javascripts/map/tutorial.js
@@ -1,17 +1,24 @@
 //= require introjs
 
+L.easyButton('fa-question-circle', function(btn, map) {
+  introJs().setOption('showProgress', true).setOption("showStepNumbers", false).start();
+}).addTo(map);
+
 $(".leaflet-control-zoom")
   .attr("data-intro", "Oi")
   .attr("data-step", "3")
   .attr("data-position", "auto");
-$(".leaflet-control-layers.leaflet-control")
-  .attr("data-intro", "Filtro")
+$(".leaflet-bar.easy-button-container.leaflet-control")
+  .attr("data-intro", "Rota")
   .attr("data-step", "4")
   .attr("data-position", "auto");
-$(".leaflet-bar.easy-button-container.leaflet-control")
-.attr("data-intro", "Rota")
-.attr("data-step", "5")
-.attr("data-position", "auto");
-//$("a.introjs-button.introjs-skipbutton")
+  $(".leaflet-bar.easy-button-container.leaflet-control")
+    .attr("data-intro", "Rota")
+    .attr("data-step", "6")
+    .attr("data-position", "auto");
+$(".leaflet-control-layers.leaflet-control")
+  .attr("data-intro", "Filtro")
+  .attr("data-step", "5")
+  .attr("data-position", "auto");
 
-introJs().setOption('showProgress', true).start();
+introJs().setOption('showProgress', true).setOption("showStepNumbers", false).start();
