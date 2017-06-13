74f70879091316f9e63e3550c9aa668df4f43ef5
Mateus Roriz <mateusvroriz6b@gmail.com>
2017-06-13 17:56:06 +0000

    Fix the crash problem with admin_controller_spec

--- darcyWeb/spec/controllers/admin_controller_spec.rb
+++ darcyWeb/spec/controllers/admin_controller_spec.rb
@@ -1,6 +1,9 @@
 # TODO Substituir por testes de aceitacao
-# require 'rails_helper'
-#
+# TODO Fix the problem with needing an admin before creation
+require 'rails_helper'
+require 'spec_helper'
+require 'json'
+
 # describe AdminController,  type: :controller do
 #
 # 	before do
@@ -9,8 +12,19 @@
 #
 # 	describe "GET new" do
 # 		it "should GET a new user" do
-# 			get :new
-# 			expect(response). to have_http_status(:sucess)
+# 			get 'admin'
+# 			response.should be_success
 # 		end
 # 	end
 # end
+
+
+RSpec.describe AdminController, :type => :controller do
+  describe "GET admin" do
+    it "should get a new admin" do
+			admin = FactoryGirl.create(:admin)
+      get :admin
+      expect(assigns(:admin)).to eq([admin])
+    end
+  end
+end
