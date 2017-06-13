7e6be6b9b28cc7a1f974666156d7e1185431f31d
Stéfane Souza <stefanesouza04@gmail.com>
2017-06-13 14:44:57 -0300

    Creating plans model and view.

---
+++ app/models/plans.rb
@@ -0,0 +1,7 @@
+class Plans < ApplicationRecord
+  belongs_to :buildings
+  validates :building, presence: true
+  validates :level, length: { maximum: 3 }, numericality: { only_integer: true }
+  validates :geo_data, presence: true
+  validates :image, presence: true
+end
