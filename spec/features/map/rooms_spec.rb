3d76bd14ad42d94ab2f6dabccfa6171407f0a7a5
Kairon Velozo <kairon333@gmail.com>
2017-06-13 19:39:42 -0300

    Testing shows schedules when the user clicks in a room.

--- spec/features/map/rooms_spec.rb
+++ spec/features/map/rooms_spec.rb
@@ -3,9 +3,12 @@ require 'rails_helper'
 describe 'Map', type: :feature do
 
   before(:each) do
-    unless RSpec.current_example.metadata[:no_rooms]
+    if RSpec.current_example.metadata[:room]
       # Creates the room and its associated building
       FactoryGirl.create :room
+    elsif RSpec.current_example.metadata[:course]
+      # Creates the room and its associated building
+      FactoryGirl.create :course
     else
       # Creates a building (for cases when there are no rooms)
       FactoryGirl.create :building
@@ -20,7 +23,7 @@ describe 'Map', type: :feature do
 
   context 'when there are rooms' do
 
-    it 'should show the indoor map when click on building', js: true do
+    it 'should show the indoor map when click on building', js: true, room: true do
 
       # Get the number of layers on the map before click in a building
       layers_before = page.evaluate_script("Object.keys(map._layers).length")
@@ -34,7 +37,7 @@ describe 'Map', type: :feature do
       expect(layers_after).to be > layers_before
     end
 
-    it 'should show the building level control when click on building', js: true do
+    it 'should show the building level control when click on building', js: true, room: true do
 
       page.execute_script("buildingLayer.getLayers()[0].fire('click')")
       wait_for_ajax
@@ -43,7 +46,7 @@ describe 'Map', type: :feature do
       expect(level_control).to eq(true)
     end
 
-    it 'should show the rooms table in the sidebar when click on building', js: true do
+    it 'should show the rooms table in the sidebar when click on building', js: true, room: true do
 
       page.execute_script("buildingLayer.getLayers()[0].fire('click')")
       wait_for_ajax
@@ -51,11 +54,24 @@ describe 'Map', type: :feature do
       expect(page).to have_css("#sidebar table")
     end
 
+    it 'should show the schedule when click on room', js: true, course: true do
+
+      # clicks the building layer feature
+      page.execute_script("buildingLayer.getLayers()[0].fire('click')")
+      wait_for_ajax
+
+      page.execute_script("var layers = indoorLayer._layers[0]._layers; layers[Object.keys(layers)[0]].fire('click')")
+      wait_for_ajax
+
+      expect(page).to have_css("#sidebar .nav-tabs")
+
+    end
+
   end
 
   context 'when there are no rooms' do
 
-    it 'should not show the indoor map when click on building', js: true, no_rooms: true do
+    it 'should not show the indoor map when click on building', js: true, building: true do
 
       # Get the number of layers on the map before click in a building
       layers_before = page.evaluate_script("Object.keys(map._layers).length")
@@ -69,7 +85,7 @@ describe 'Map', type: :feature do
       expect(layers_after).to be(layers_before)
     end
 
-    it 'should not show the building level control when click on building', js: true, no_rooms: true do
+    it 'should not show the building level control when click on building', js: true, building: true do
 
       page.execute_script("buildingLayer.getLayers()[0].fire('click')")
       wait_for_ajax
@@ -78,7 +94,7 @@ describe 'Map', type: :feature do
       expect(level_control).to eq(false)
     end
 
-    it 'should show alert in the sidebar when click on building', js: true, no_rooms: true do
+    it 'should show alert in the sidebar when click on building', js: true, building: true do
 
       page.execute_script("buildingLayer.getLayers()[0].fire('click')")
       wait_for_ajax
