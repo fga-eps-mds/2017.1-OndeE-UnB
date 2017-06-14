1391f56fb450080e363d0bf9fbfa5fb0485e972d
Kairon Velozo <kairon333@gmail.com>
2017-06-14 20:24:30 -0300

    Changing tests code.

--- spec/controllers/admin_controller_spec.rb
+++ spec/controllers/admin_controller_spec.rb
@@ -4,27 +4,16 @@ require 'rails_helper'
 require 'spec_helper'
 require 'json'
 
-# describe AdminController,  type: :controller do
-#
-# 	before do
-# 		@admin = FactoryGirl.create(:admin)
-# 	end
-#
-# 	describe "GET new" do
-# 		it "should GET a new user" do
-# 			get 'admin'
-# 			response.should be_success
-# 		end
-# 	end
-# end
+describe AdminController,  type: :controller do
 
+	before do
+		@admin = FactoryGirl.create(:admin)
+	end
 
-RSpec.describe AdminController, :type => :controller do
-  describe "GET admin" do
-    it "should get a new admin" do
-			admin = FactoryGirl.create(:admin)
-      get :admin
-      expect(assigns(:admin)).to eq([admin])
-    end
-  end
+	describe "GET new" do
+		pending "should GET a new user" do
+			get 'admin'
+			response.should be_success
+		end
+	end
 end
