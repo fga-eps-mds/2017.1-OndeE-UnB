12fc2a5c73730aed50559f56f66c5bcc8e281637
Sannya Arvelos <sannyasantana@gmail.com>
2017-06-14 12:35:03 -0300

    Setting typeahead in the routes form

--- app/assets/javascripts/map/search.js
+++ app/assets/javascripts/map/search.js
@@ -1,4 +1,5 @@
 //= require leaflet/geojsonautocomplete
+//= require jquery/typeahead
 
 $(document).ready(function() {
 
@@ -8,27 +9,60 @@ $(document).ready(function() {
   $("#searchContainer").GeoJsonAutocomplete(options);
 
   sidebar.on('show', function() {
-    $('#route_origin, #route_destination').hide();
-    $('.input-location').GeoJsonAutocomplete(options);
+    var locations = new Bloodhound({
+      datumTokenizer: Bloodhound.tokenizers.obj.whitespace('title'),
+      queryTokenizer: Bloodhound.tokenizers.whitespace,
+      prefetch: '/map/search',
+      remote: {
+        url: '/map/search',
+        wildcard: '%QUERY'
+      }
+    });
 
-    $('.searchButton, .divider, .clearButton').remove();
-
-    $('.input-location').each(function(index, input){
-      $(input).on('DOMSubtreeModified', function() {
-        $(this).find('li').on('mouseenter', function() {
-            var latLng = L.latLng($(this).data('latitude'), $(this).data('longitude'));
-            latLng['latlng'] = {
-              lat: latLng.lat,
-              lng: latLng.lng
-            };
-            if($(input).hasClass('origin')){
-              routesFromHere(latLng);
-            } else {
-              routesToHere(latLng);
-            }
-
-        });
-      });
+    $('.input-location .origin, .input-location .destination').typeahead({
+      hint: true,
+      highlight: true,
+      minLength: 1
+    }, {
+      name: 'locations',
+      display: 'title',
+      source: locations
     });
   });
 });
+//   sidebar.on('show', function() {
+//     $('#route_origin, #route_destination').hide();
+//     $('.input-location').GeoJsonAutocomplete(options);
+//
+//     $('#sidebar').find('.searchButton, .divider, .clearButton').remove();
+//     $('#sidebar').find('.searchContainer').removeClass('searchContainer');
+//     $('#sidebar').find('.searchBox').removeClass('searchBox').addClass('form-control');
+//
+//     $('#sidebar .form-control').find(".input-location.origin")
+//       .find(".form-control").attr("autocomplete", "off").attr("placeholder", "Partindo de onde?");
+//
+//       $('#sidebar .form-control').find(".input-location.destination")
+//         .find(".form-control").attr("autocomplete", "off").attr("placeholder", "Para onde?");
+//
+//     $('.input-location').each(function(index, input){
+//       $(input).on('DOMSubtreeModified', function() {
+//         console.log($(this).find("ul").html())
+//         $(this).find("li").each(function(index, listItem)){
+//
+//           $(listItem)
+//             var latLng = L.latLng($(this).data('latitude'), $(this).data('longitude'));
+//             latLng['latlng'] = {
+//               lat: latLng.lat,
+//               lng: latLng.lng
+//             };
+//             if($(input).hasClass('origin')){
+//               routesFromHere(latLng);
+//             } else {
+//               routesToHere(latLng);
+//             }
+//
+//         });
+//       });
+//     });
+//   });
+// });
