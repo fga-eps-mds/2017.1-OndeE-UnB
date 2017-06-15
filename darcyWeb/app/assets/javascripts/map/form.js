8d9e4c07baf0cb67c72d9c4eaf5ffeee7935f29b
MatheusMello <mat.mello93@gmail.com>
2017-06-14 21:47:24 -0300

    refactoring

--- darcyWeb/app/assets/javascripts/map/form.js
+++ darcyWeb/app/assets/javascripts/map/form.js
@@ -5,6 +5,13 @@ class Form {
 
   constructor() {
     this.name = 'Form Object';
+
+    this.form = $('#sidebar').find('form');
+    this.origin = this.form.find('input[name="route[origin]"]');
+    this.destination = this.form.find('input[name="route[destination]"]');
+    this.mode = this.form.find('input[name="route[mode]"]');
+    this.submit = this.form.find('button[type=submit]');
+
   }
 
 }
