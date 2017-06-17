eedee1465d3dc388685d722c427a4c098a0b0bd1
Sannya Arvelos <sannyasantana@gmail.com>
2017-06-17 00:33:22 -0300

    Applying template autocomplete

--- db/schema.rb
+++ db/schema.rb
@@ -12,7 +12,6 @@
 
 ActiveRecord::Schema.define(version: 20170614011033) do
 
-
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -32,6 +31,13 @@ ActiveRecord::Schema.define(version: 20170614011033) do
     t.datetime "updated_at",                          null: false
     t.index ["email"], name: "index_admins_on_email", unique: true, using: :btree
     t.index ["reset_password_token"], name: "index_admins_on_reset_password_token", unique: true, using: :btree
+  end
+
+  create_table "articles", force: :cascade do |t|
+    t.string   "title"
+    t.text     "content"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "buildings", force: :cascade do |t|
@@ -78,7 +84,6 @@ ActiveRecord::Schema.define(version: 20170614011033) do
     t.integer "level"
     t.index ["building_id"], name: "index_rooms_on_building_id", using: :btree
   end
-
 
   create_table "schedules", force: :cascade do |t|
     t.integer "room_id"
