348c78352ed7e5c74d0626b83fe257ef1d87be21
rafaelrabetti <rafael.rabetti@gmail.com>
2017-06-20 15:28:14 -0300

    Fixing codeclimate issues.

--- darcyWeb/app/controllers/admin/admins_controller.rb
+++ darcyWeb/app/controllers/admin/admins_controller.rb
@@ -1,61 +1,66 @@
 class Admin::AdminsController < AdminController
   before_action :set_admin, only: [:destroy, :edit, :update]
 
-    def index
-        @admins = Admin.all
-    end
+  def index
+    @admins = Admin.all
+  end
 
-    def new
-        @admin = Admin.new
-    end
+  def new
+    @admin = Admin.new
+  end
 
-    def create
-        @admin = Admin.new(admin_params)
-        if @admin.save
-          redirect_to admin_admins_path, notice: helpers.alert_success('Administrador criado com êxito.')
-        else
-          render :new
-        end
+  def create
+    @admin = Admin.new(admin_params)
+    if @admin.save
+      redirect_to admin_admins_path,
+      notice: helpers.alert_success('Administrador criado com êxito.')
+    else
+      render :new
     end
+  end
 
-    def edit
-      @admin = Admin.find(params[:id])
-    end
+  def edit
+    @admin = Admin.find(params[:id])
+  end
 
-    def update
-      @admin = Admin.find(params[:id])
-      if @admin.update(admin_params_edit)
-        redirect_to admin_admins_path, notice: helpers.alert_success('Administrador editado com êxito.')
-      else
-        flash.now[:alert] = "Member not updated"
-        render 'edit'
-      end
+  def update
+    @admin = Admin.find(params[:id])
+    if @admin.update(admin_params_edit)
+      redirect_to admin_admins_path,
+      notice: helpers.alert_success('Administrador editado com êxito.')
+    else
+      flash.now[:alert] = 'Member not updated'
+      render 'edit'
     end
+  end
 
-    def my_requests
-      @admin = current_user
-    end
+  def my_requests
+    @admin = current_user
+  end
 
-    def destroy
-      user = Admin.find(params[:id])
-      if (current_admin == user)
-        redirect_to admin_admins_path, notice: helpers.alert_danger('Você não pode se excluir.')
-      else
-      @admin.destroy
-      redirect_to admin_admins_path, notice: helpers.alert_success('Administrador excluído com êxito.')
+  def destroy
+    user = Admin.find(params[:id])
+    if current_admin == user
+      redirect_to admin_admins_path,
+      notice: helpers.alert_danger('Você não pode se excluir.')
+    else
+    @admin.destroy
+    redirect_to admin_admins_path,
+    notice: helpers.alert_success('Administrador excluído com êxito.')
     end
-    end
+  end
 
     private
-    def set_admin
-      @admin = Admin.find(params[:id])
-    end
 
-    def admin_params
-      params.require(:admin).permit(:name, :email, :password, :password_confirmation)
-    end
+  def set_admin
+    @admin = Admin.find(params[:id])
+  end
 
-    def admin_params_edit
-      params.require(:admin).permit(:name, :email)
-    end
+  def admin_params
+    params.require(:admin).permit(:name, :email, :password, :password_confirmation)
+  end
+
+  def admin_params_edit
+    params.require(:admin).permit(:name, :email)
+  end
 end
--- darcyWeb/app/controllers/search_controller.rb
+++ darcyWeb/app/controllers/search_controller.rb
@@ -1,25 +1,25 @@
 class SearchController < MapController
 
   def index
-     buildings = json_building_search(params[:search])
-     departments = json_department_search(params[:search])
-     rooms = json_room_search(params[:search])
-     features = buildings + departments + rooms
-     geo_json = {
-      type: "FeatureCollection",
+    buildings = json_building_search(params[:search])
+    departments = json_department_search(params[:search])
+    rooms = json_room_search(params[:search])
+    features = buildings + departments + rooms
+    geo_json = {
+      type: 'FeatureCollection',
       features: features
-     }
-     render plain: geo_json.to_json
+    }
+    render plain: geo_json.to_json
   end
 
   def json_room_search(search)
     search = "%#{search}%"
-    @room = Room.joins(:location).where("locations.title ILIKE ? OR acronym ILIKE ? ", search, search)
+    room_search_string = 'locations.title ILIKE ? OR acronym ILIKE ? '
+    @room = Room.joins(:location).where(room_search_string, search, search)
     features = []
-    @room.each_with_index do |room, index|
-
+    @room.each_with_index do |room, _index|
       properties = {
-        popupContent: "MDS",
+        popupContent: 'MDS',
         title: room.title,
         description: room.room_type,
         image: 'fa-room'
@@ -27,7 +27,7 @@ class SearchController < MapController
 
       geo_data = JSON.parse room.geo_data
       geo_data['features'].each do |feature|
-        feature.merge!(properties: properties)
+        feature[:properties] = properties
         features.push(feature)
       end
     end
@@ -36,12 +36,12 @@ class SearchController < MapController
 
   def json_department_search(search)
     search = "%#{search}%"
-    @department = Department.joins(:location).where("locations.title ILIKE ? OR acronym ILIKE ? ", search, search)
+    department_search_string = 'locations.title ILIKE ? OR acronym ILIKE ? '
+    @department = Department.joins(:location).where(department_search_string, search, search)
     features = []
-    @department.each_with_index do |department, index|
-
+    @department.each_with_index do |department, _index|
       properties = {
-        popupContent: "MDS",
+        popupContent: 'MDS',
         title: department.acronym,
         description: department.title,
         image: 'fa-department'
@@ -49,7 +49,7 @@ class SearchController < MapController
 
       geo_data = JSON.parse department.geo_data
       geo_data['features'].each do |feature|
-        feature.merge!(properties: properties)
+        feature[:properties] = properties
         features.push(feature)
       end
     end
@@ -58,12 +58,12 @@ class SearchController < MapController
 
   def json_building_search(search)
     search = "%#{search}%"
-    @buildings = Building.joins(:location).where("locations.title ILIKE ? OR acronym ILIKE ? ", search, search)
+    building_search_string = 'locations.title ILIKE ? OR acronym ILIKE ? '
+    @buildings = Building.joins(:location).where(building_search_string, search, search)
     features = []
-    @buildings.each_with_index do |building, index|
-
+    @buildings.each_with_index do |building, _index|
       properties = {
-        popupContent: "MDS",
+        popupContent: 'MDS',
         title: building.acronym,
         description: building.title,
         image: 'fa-building'
@@ -71,10 +71,9 @@ class SearchController < MapController
 
       geo_data = JSON.parse building.geo_data
       geo_data['features'].each do |feature|
-        feature.merge!(properties: properties)
+        feature[:properties] = properties
         features.push(feature)
       end
-
     end
     features
   end
