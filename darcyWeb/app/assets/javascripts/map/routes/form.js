dbdc9a88a07c2d499bd1de14661ac7fd290226e0
Sannya Arvelos <sannyasantana@gmail.com>
2017-06-20 15:03:43 -0300

    Organizing routes js files.

---
+++ darcyWeb/app/assets/javascripts/map/routes/form.js
@@ -0,0 +1,12 @@
+//= require leaflet/routing-machine
+//= require leaflet/lrm-mapzen
+
+class Form {
+  constructor() {
+    this.form = $('#sidebar').find('form');
+    this.origin = this.form.find('input[name="route[origin]"]');
+    this.destination = this.form.find('input[name="route[destination]"]');
+    this.mode = this.form.find('input[name="route[mode]"]');
+    this.submit = this.form.find('button[type=submit]');
+  }
+}
