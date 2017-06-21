997688f6b2ba6db7411783b8d966683b1d23c7f0
Kairon Velozo <kairon333@gmail.com>
2017-06-21 14:19:40 -0300

    Setting autocomplete for the routes form fields.

--- darcyWeb/app/assets/javascripts/map/search.js
+++ darcyWeb/app/assets/javascripts/map/search.js
@@ -7,42 +7,4 @@ $(document).ready(function() {
     geojsonServiceAddress: "/map/search/geo"
   };
   $("#searchContainer").GeoJsonAutocomplete(options);
-
-  sidebar.on('show', function() {
-    var locations = new Bloodhound({
-      datumTokenizer: Bloodhound.tokenizers.obj.whitespace('title'),
-      queryTokenizer: Bloodhound.tokenizers.whitespace,
-      prefetch: '/map/search',
-      remote: {
-        url: '/map/search?search=%QUERY',
-        wildcard: '%QUERY'
-      }
-    });
-
-    $('.input-location .origin, .input-location .destination').typeahead({
-      hint: true,
-      highlight: true,
-      minLength: 1
-    }, {
-      name: 'locations',
-      display: 'title',
-      source: locations,
-      templates: {
-        suggestion: function(data) {
-          return '<p><strong>' + data.acronym + '</strong> - ' + data.title + '</p>';
-        }
-      }
-    }).bind('typeahead:select', function(ev, suggestion) {
-      var latLng = L.latLng(suggestion.latitude, suggestion.longitude);
-      latLng['latlng'] = {
-        lat: latLng.lat,
-        lng: latLng.lng
-      };
-      if ($(this).hasClass('origin')) {
-        routesFromHere(latLng);
-      } else {
-        routesToHere(latLng);
-      }
-    });
-  });
 });
