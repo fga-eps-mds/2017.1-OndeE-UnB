9a9ff8a365a9765590e6e02c76c2e5a78fed6119
Sannya Arvelos <sannyasantana@gmail.com>
2017-06-21 23:09:27 -0300

    uploading image to S3

--- config/initializers/shrine.rb
+++ config/initializers/shrine.rb
@@ -1,10 +1,18 @@
 require "shrine"
-require "shrine/storage/file_system"
+require "shrine/storage/s3"
 
-Shrine.storages = {
-  cache: Shrine::Storage::FileSystem.new("public", prefix: "uploads/cache"),
-  store: Shrine::Storage::FileSystem.new("public", prefix: "uploads/store"),
+s3_options = {
+ access_key_id:     ENV['AWS_ACCESS_KEY_ID'],
+ secret_access_key: ENV['AWS_SECRET_ACCESS_KEY'],
+ region:            "us-west-2",
+ bucket:            ENV['AWS_S3_BUCKET'],
 }
 
+Shrine.storages = {
+ cache: Shrine::Storage::S3.new(prefix: "cache", **s3_options),
+ store: Shrine::Storage::S3.new(prefix: "store", **s3_options)
+}
+
+
 Shrine.plugin :activerecord
-Shrine.plugin :cached_attachment_data
+Shrine.plugin :cached_attachment_data # for forms
