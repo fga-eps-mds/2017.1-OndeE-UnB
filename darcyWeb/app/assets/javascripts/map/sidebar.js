43fd8708a819f4dcf74d6eed65dafaa48a162f0f
Sannya Arvelos <sannyasantana@gmail.com>
2017-06-24 16:14:25 -0300

    Seting building image in the sidebar.

--- darcyWeb/app/assets/javascripts/map/sidebar.js
+++ darcyWeb/app/assets/javascripts/map/sidebar.js
@@ -6,11 +6,8 @@ var sidebar = L.control.sidebar("sidebar", {
     closeButton: false
 }).addTo(map);
 
-//var visible = sidebar.isVisible();
-
 sidebar.on("show", function(){
   $(".close").click(function(){
     sidebar.hide();
   });
-  //$(".leaflet-map-pane").css("height", "50%");
 });
