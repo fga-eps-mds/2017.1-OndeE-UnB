e5184d4e8fa2c23b73be4178c09af21b72adef69
Sannya Arvelos <sannyasantana@gmail.com>
2017-11-04 01:08:40 -0200

    Refactoring buildingsLayer to es6.

--- darcyWeb/app/javascript/map/buildingsLayer.js
+++ darcyWeb/app/javascript/map/buildingsLayer.js
@@ -1,4 +1,4 @@
-const buildingLayer = L.geoJSON(false, {
+const buildingsLayer = L.geoJSON(false, {
   onEachFeature: (feature, layer) => {
     layer.setStyle({
       fillColor: buildingStyles[mapOptions.baseLayer].color,
@@ -7,27 +7,27 @@ const buildingLayer = L.geoJSON(false, {
       weight: 1.5
     });
 
-    layer.on("add", function(ev){
+    layer.on("add", ev => {
 
-      var content = layer.feature.properties.building.acronym;
+      const content = layer.feature.properties.building.acronym;
       // Define the offset of the label based on the word length
       tooltipOptions.offset[0] = -(content.length) * acronymTooltipOffset;
 
-      layer.bindTooltip(function(layer) {
+      layer.bindTooltip(layer => {
         return content;
       }, tooltipOptions);
     });
     // Trigger when user click on a building
-    layer.on("click", function() {
+    layer.on("click", () => {
       // The key references to that building clicked
-      var buildingID = this.feature.properties.building.id;
+      const buildingID = this.feature.properties.building.id;
 
       if (sidebar.isVisible()) {
         sidebar.hide();
       } else {
         //selects the building clicked and shows sidebar
-        var numberToBuilding = "/map/data/building/" + buildingID;
-        $("#sidebar").load(numberToBuilding, function() {
+        const numberToBuilding = "/map/data/building/" + buildingID;
+        $("#sidebar").load(numberToBuilding, () => {
           sidebar.toggle();
         });
         // Load rooms for clicked building
@@ -36,3 +36,5 @@ const buildingLayer = L.geoJSON(false, {
     });
   }
 }).addTo(map);
+
+export default buildingsLayer;
